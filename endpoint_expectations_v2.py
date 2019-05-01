cdcc241863d28415d6b3c012c2e3417cc2001758
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-05-01 10:01:33 -0400

    Update endpoint_expectations_v2.py

--- endpoint_expectations_v2.py
+++ endpoint_expectations_v2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 
 def estimate_expected_endpoints(monthly_mu, monthly_sigma, num_patients_per_trial, num_trials_per_bin,
@@ -232,6 +233,11 @@ def generate_expected_endpoint_maps(monthly_mu_axis_start, monthly_mu_axis_stop,
             # actually calculate the expected RR50 as well as the expected MPC
             [expected_RR50, expected_MPC] = estimate_expected_endpoints(monthly_mu, monthly_sigma, num_patients_per_trial, num_trials_per_bin,
                                                                         num_months_per_patient_baseline, num_months_per_patient_testing)
+            
+            print('\n\nmonthly mean: '             + str(monthly_mu)    + 
+                    '\nmonthly standard deviation: ' + str(monthly_sigma) + 
+                    '\nexpected RR50: '            + str(expected_RR50) + 
+                    '\nexpected MPC: '             + str(expected_MPC)    )
             
             # store the expected RR50 and expected MPC into their respective 2D arrays
             expected_RR50_map[monthly_sigma_index, monthly_mu_index] = expected_RR50
@@ -485,7 +491,7 @@ def generate_SNR_data(shape_1, scale_1, alpha_1, beta_1, shape_2, scale_2, alpha
     # calculate the histogram of the Model 2 patients
     [H_model_2, _, _] = np.histogram2d(model_2_monthly_count_averages, model_2_monthly_count_standard_deviations, bins=[ny, nx], 
                                     range=[[monthly_mu_axis_start, monthly_mu_axis_stop], [monthly_sigma_axis_start, monthly_sigma_axis_stop]])
-    H_model_2 = np.flipud(np.fliplr(np.transpose(np.flipud(H_model_1))))
+    H_model_2 = np.flipud(np.fliplr(np.transpose(np.flipud(H_model_2))))
     norm_const_2 = np.sum(np.sum(H_model_2, 0))
     H_model_2 = H_model_2/norm_const_2
 
@@ -528,7 +534,7 @@ scale_2 = 296.728
 alpha_2 = 296.339
 beta_2 = 243.719
 
-num_patients_per_model = 1000
+num_patients_per_model = 10000
 num_months_per_patient = 24
 num_days_per_month = 28
 
@@ -538,6 +544,15 @@ min_power_law_slope = 0.5
 power_law_slope_spacing = 0.1
 legend_decimal_round = 1
 
+# picture file names
+expected_RR50_filename = 'expected_RR50'
+expected_MPC_filename = 'expected_MPC'
+expected_RR50_with_curves_filename = 'expected_RR50_with_power_law_curves'
+expected_MPC_with_curves_filename = 'expected_MPC_with_power_law_curves'
+model_1_2D_hist_filename = 'Model_1_2D_histogram'
+model_2_2D_hist_filename = 'Model_2_2D_histogram'
+
+# calculate the expected enpoint response maps, the Model 1 and Model 2 histograms, and the 
 [num_monthly_mu, num_monthly_sigma, monthly_mu_array, 
  Model_1_expected_RR50, Model_1_expected_MPC, Model_2_expected_RR50, Model_2_expected_MPC, 
  expected_RR50_map, expected_MPC_map, H_model_1, H_model_2] = \
@@ -548,35 +563,68 @@ legend_decimal_round = 1
                         num_patients_per_trial, num_trials_per_bin,
                         num_months_per_patient_baseline, num_months_per_patient_testing)
 
+# generate the tick labels for the monthly mean axis as well as for the monthly standard deviation axis
+# the tick labels are distinctly different from the actual ticks
 monthly_mu_tick_labels = np.arange(monthly_mu_axis_start, monthly_mu_axis_stop + monthly_mu_tick_spacing, monthly_mu_tick_spacing)
 monthly_sigma_tick_labels = np.arange(monthly_sigma_axis_start, monthly_sigma_axis_stop + monthly_sigma_tick_spacing, monthly_sigma_tick_spacing)
 
+# get the number of tick labels for both axes
 num_monthly_mu_tick_labels = len(monthly_mu_tick_labels)
 num_monthly_sigma_tick_labels = len(monthly_sigma_tick_labels)
 
+# calculate the ticks (location of each tick) for the monthly mean axis and monthly standard deviation axis
 monthly_mu_ticks = monthly_mu_tick_labels/monthly_mu_axis_step + 0.5*np.ones(num_monthly_mu_tick_labels)
 monthly_sigma_ticks = np.flip( monthly_sigma_tick_labels/monthly_sigma_axis_step + 0.5*np.ones(num_monthly_sigma_tick_labels), 0)
 
+# plot the expected RR50
 plt.figure()
-ax = sns.heatmap(expected_RR50_map)
+ax = sns.heatmap(expected_RR50_map, cbar_kws={'label':'estimated expected endpoint response in percentages'})
 ax.set_xticks(monthly_mu_ticks)
 ax.set_xticklabels(monthly_mu_tick_labels, rotation='horizontal')
 ax.set_yticks(monthly_sigma_ticks)
 ax.set_yticklabels(monthly_sigma_tick_labels, rotation='horizontal')
+plt.xlabel('monthly seizure count mean')
+plt.ylabel('monthly seizure count standard deviation')
+plt.title('Expected RR50 Placebo')
+plt.savefig(os.getcwd + '/' + expected_RR50_filename + '.png')
 
+# plot the expected MPC
 plt.figure()
-ax = sns.heatmap(expected_MPC_map)
+ax = sns.heatmap(expected_MPC_map, cbar_kws={'label':'estimated expected endpoint response in percentages'})
 ax.set_xticks(monthly_mu_ticks)
 ax.set_xticklabels(monthly_mu_tick_labels, rotation='horizontal')
 ax.set_yticks(monthly_sigma_ticks)
 ax.set_yticklabels(monthly_sigma_tick_labels, rotation='horizontal')
+plt.xlabel('monthly seizure count mean')
+plt.ylabel('monthly seizure count standard deviation')
+plt.title('Expected MPC Placebo')
+plt.savefig(os.getcwd + '/' + expected_MPC_filename + '.png')
 
+# get the scaling ratios for any other data that needs to be plotted on top of the heatmap
 monthly_mu_scale_ratio = num_monthly_mu/(monthly_mu_axis_stop - monthly_mu_axis_start)
 monthly_sigma_scale_ratio = num_monthly_sigma/(monthly_sigma_axis_stop - monthly_sigma_axis_start)
 power_law_slopes = np.arange(min_power_law_slope/power_law_slope_spacing, max_power_law_slope/power_law_slope_spacing + 1, 1)*power_law_slope_spacing
 
+# plot the expected RR50 with corresponding power law curves
 plt.figure()
-ax = sns.heatmap(expected_RR50_map)
+ax = sns.heatmap(expected_RR50_map, cbar_kws={'label':'estimated expected endpoint response in percentages'})
+ax.set_xticks(monthly_mu_ticks)
+ax.set_xticklabels(monthly_mu_tick_labels, rotation='horizontal')
+ax.set_yticks(monthly_sigma_ticks)
+ax.set_yticklabels(monthly_sigma_tick_labels, rotation='horizontal')
+for power_law_slope_index in range(len(power_law_slopes)):
+        power_law_slope = power_law_slopes[power_law_slope_index]
+        power_law_monthly_sigma_array = np.power(monthly_mu_array, power_law_slope)
+        plt.plot(monthly_mu_array*monthly_mu_scale_ratio, (monthly_sigma_axis_stop - power_law_monthly_sigma_array)*monthly_sigma_scale_ratio )
+plt.legend( [ str(np.round(power_law_slopes[power_law_slope_index], legend_decimal_round)) for power_law_slope_index in range(len(power_law_slopes)) ] )
+plt.xlabel('monthly seizure count mean')
+plt.ylabel('monthly seizure count standard deviation')
+plt.title('Expected RR50 Placebo')
+plt.savefig(os.getcwd + '/' + expected_RR50_with_curves_filename + '.png')
+
+# plot the expected MPC with corresponding power law curves
+plt.figure()
+ax = sns.heatmap(expected_MPC_map, cbar_kws={'label':'estimated expected endpoint response in percentages'})
 ax.set_xticks(monthly_mu_ticks)
 ax.set_xticklabels(monthly_mu_tick_labels, rotation='horizontal')
 ax.set_yticks(monthly_sigma_ticks)
@@ -586,17 +634,33 @@ for power_law_slope_index in range(len(power_law_slopes)):
         power_law_monthly_sigma_array = np.power(monthly_mu_array, power_law_slope)
         plt.plot(monthly_mu_array*monthly_mu_scale_ratio, (monthly_sigma_axis_stop - power_law_monthly_sigma_array)*monthly_sigma_scale_ratio )
 plt.legend( [ str(np.round(power_law_slopes[power_law_slope_index], legend_decimal_round)) for power_law_slope_index in range(len(power_law_slopes)) ] ) 
+plt.xlabel('monthly seizure count mean')
+plt.ylabel('monthly seizure count standard deviation')
+plt.title('Expected MPC Placebo')
+plt.savefig(os.getcwd + '/' + expected_MPC_with_curves_filename + '.png')
 
+# plot the 2D histogram of Model 1 patients
 plt.figure()
-ax = sns.heatmap(expected_MPC_map)
+ax = sns.heatmap(H_model_1, cbar_kws={'label':'probability of sampling patient from a point'})
 ax.set_xticks(monthly_mu_ticks)
 ax.set_xticklabels(monthly_mu_tick_labels, rotation='horizontal')
 ax.set_yticks(monthly_sigma_ticks)
 ax.set_yticklabels(monthly_sigma_tick_labels, rotation='horizontal')
-for power_law_slope_index in range(len(power_law_slopes)):
-        power_law_slope = power_law_slopes[power_law_slope_index]
-        power_law_monthly_sigma_array = np.power(monthly_mu_array, power_law_slope)
-        plt.plot(monthly_mu_array*monthly_mu_scale_ratio, (monthly_sigma_axis_stop - power_law_monthly_sigma_array)*monthly_sigma_scale_ratio )
-plt.legend( [ str(np.round(power_law_slopes[power_law_slope_index], legend_decimal_round)) for power_law_slope_index in range(len(power_law_slopes)) ] ) 
+plt.xlabel('monthly seizure count mean')
+plt.ylabel('monthly seizure count standard deviation')
+plt.title('Model 1 patient population')
+plt.savefig(os.getcwd + '/' + model_1_2D_hist_filename + '.png')
 
-plt.show()
+# plot the 2D histogram of Model 2 patients
+plt.figure()
+ax = sns.heatmap(H_model_2, cbar_kws={'label':'probability of sampling patient from a point'})
+ax.set_xticks(monthly_mu_ticks)
+ax.set_xticklabels(monthly_mu_tick_labels, rotation='horizontal')
+ax.set_yticks(monthly_sigma_ticks)
+ax.set_yticklabels(monthly_sigma_tick_labels, rotation='horizontal')
+plt.xlabel('monthly seizure count mean')
+plt.ylabel('monthly seizure count standard deviation')
+plt.title('Model 2 patient population')
+plt.savefig(os.getcwd + '/' + model_2_2D_hist_filename + '.png')
+
+
