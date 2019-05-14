b50ea86a6776510da9e9b049b160313d4aaaee7c
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-05-14 14:09:15 -0400

    updated gitignore and changed documentation

--- generate_data.py
+++ generate_data.py
@@ -282,7 +282,23 @@ def generate_expected_endpoint_maps(start_monthly_mean,       stop_monthly_mean,
                                     start_monthly_std_dev,    stop_monthly_std_dev, step_monthly_std_dev,
                                     num_baseline_months,      num_testing_months,   min_req_base_sz_count, 
                                     num_patients_per_trial,   num_trials):
+    '''
 
+    Inputs:
+
+        1) start_monthly_mean:
+        stop_monthly_mean:
+        step_monthly_mean: 
+        start_monthly_std_dev:
+        stop_monthly_std_dev:
+        step_monthly_std_dev:
+        num_baseline_months:
+        num_testing_months:
+        min_req_base_sz_count: 
+        num_patients_per_trial:
+        num_trials
+
+    '''
     # create the monthly mean and monthly standard deviation axes
     monthly_mean_array = np.arange(start_monthly_mean, stop_monthly_mean + step_monthly_mean, step_monthly_mean)
     monthly_std_dev_array = np.arange(start_monthly_std_dev, stop_monthly_std_dev + step_monthly_std_dev, step_monthly_std_dev)
@@ -347,6 +363,7 @@ def store_map(data_map,
               x_axis_start, x_axis_stop, x_axis_step,
               y_axis_start, y_axis_stop, y_axis_step):
 
+    # 
     data_map_file_path = os.getcwd() + '/' + data_map_file_name + '.json'
 
     data_map_metadata_file_path = os.getcwd() + '/' + data_map_meta_data_file_name + '.json'
@@ -378,86 +395,26 @@ min_req_base_sz_count = 4
 num_patients_per_trial = 153
 num_trials = 10
 
+expected_RR50_file_name = 'expected_RR50_map'
+expected_RR50_metadata_file_name = 'expected_RR50_map_metadata'
+expected_MPC_file_name = 'expected_MPC_map'
+expected_MPC_metadata_file_name = 'expected_MPC_map_metadata'
+
 [expected_RR50_endpoint_map, expected_MPC_endpoint_map] = \
     generate_expected_endpoint_maps(start_monthly_mean,       stop_monthly_mean,    step_monthly_mean, 
                                     start_monthly_std_dev,    stop_monthly_std_dev, step_monthly_std_dev,
                                     num_baseline_months,      num_testing_months,   min_req_base_sz_count, 
                                     num_patients_per_trial,   num_trials)
 
+store_map(expected_RR50_endpoint_map, 
+          expected_RR50_file_name, expected_RR50_metadata_file_name,
+          start_monthly_mean,    stop_monthly_mean,    step_monthly_mean, 
+          start_monthly_std_dev, stop_monthly_std_dev, step_monthly_std_dev)
+
+store_map(expected_RR50_endpoint_map, 
+          expected_RR50_file_name, expected_RR50_metadata_file_name,
+          start_monthly_mean,    stop_monthly_mean,    step_monthly_mean, 
+          start_monthly_std_dev, stop_monthly_std_dev, step_monthly_std_dev)
 
 
 
-
-
-
-'''
-def generate_map(start_monthly_mu,    stop_monthly_mu,    step_monthly_mu, 
-                 start_monthly_sigma, stop_monthly_sigma, step_monthly_sigma):
-
-    monthly_mu_array = np.arange(start_monthly_mu, stop_monthly_mu + step_monthly_mu, step_monthly_mu)
-    monthly_sigma_array = np.arange(start_monthly_sigma, stop_monthly_sigma + step_monthly_sigma, step_monthly_sigma)
-
-    monthly_sigma_array = np.flip(monthly_sigma_array, 0)
-
-    num_monthly_mus = len(monthly_mu_array)
-    num_monthly_sigmas = len(monthly_sigma_array)
-
-    data_map = np.zeros((num_monthly_sigmas, num_monthly_mus))
-
-    for monthly_sigma_index in range(num_monthly_sigmas):
-
-        for monthly_mu_index in range(num_monthly_mus):
-
-            monthly_mu = monthly_mu_array[monthly_mu_index]
-            monthly_sigma = monthly_sigma_array[monthly_sigma_index]
-
-            if(monthly_sigma > np.sqrt(monthly_mu)):
-
-                data_map[monthly_sigma_index, monthly_mu_index] = monthly_mu + monthly_sigma
-            
-            else:
-                
-                data_map[monthly_sigma_index, monthly_mu_index] = np.nan
-    
-    return data_map
-
-
-def store_map(data_map, 
-              data_map_file_name, data_map_meta_data_file_name,
-              x_axis_start, x_axis_stop, x_axis_step,
-              y_axis_start, y_axis_stop, y_axis_step):
-
-    data_map_file_path = os.getcwd() + '/' + data_map_file_name + '.json'
-
-    data_map_metadata_file_path = os.getcwd() + '/' + data_map_meta_data_file_name + '.json'
-
-    metadata = np.array([x_axis_start, x_axis_stop, x_axis_step, y_axis_start, y_axis_stop, y_axis_step])
-
-    with open(data_map_file_path, 'w+') as map_storage_file:
-
-        json.dump(data_map.tolist(), map_storage_file)
-
-    with open(data_map_metadata_file_path, 'w+') as map_metadata_storage_file:
-
-        json.dump(metadata.tolist(), map_metadata_storage_file)
-
-
-start_monthly_mu = 0
-stop_monthly_mu = 16
-step_monthly_mu = 1
-
-start_monthly_sigma = 0
-stop_monthly_sigma = 16
-step_monthly_sigma = 1
-
-test_map_file_name = 'test_map'
-test_map_metadata_file_name = 'test_map_metadata'
-
-test_map = generate_map(start_monthly_mu,    stop_monthly_mu,    step_monthly_mu, 
-                        start_monthly_sigma, stop_monthly_sigma, step_monthly_sigma)
-
-store_map(test_map, 
-          test_map_file_name, test_map_metadata_file_name,
-          start_monthly_mu,    stop_monthly_mu,    step_monthly_mu,
-          start_monthly_sigma, stop_monthly_sigma, step_monthly_sigma)
-'''
