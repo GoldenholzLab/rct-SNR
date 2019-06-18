b64c7bf1d1cea12f0e27b16b595a455ed5dafc0e
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-06-18 17:06:48 -0400

    Update sum_generated_maps.py

--- sum_generated_maps.py
+++ sum_generated_maps.py
@@ -7,7 +7,7 @@ def retrieve_individual_map(directory, min_req_base_sz_count, folder, file_name)
     
 
     '''
-    folder_path = directory + '/' + str(min_req_base_sz_count) + '/' + str(folder)
+    folder_path = directory + '/' + str(min_req_base_sz_count) + '/' + folder
     file_path = folder_path + '/' + file_name + '.json'
 
     with open(file_path, 'r') as json_file:
@@ -21,7 +21,10 @@ if (__name__=='__main__'):
 
     directory = '/Users/juanromero/Documents/GitHub/rct-SNR'
     min_req_base_sz_count = 0
-    folder = 1
     file_name = 'MPC_stat_power_map'
 
-    print( retrieve_individual_map(directory, min_req_base_sz_count, folder, file_name) )
+    final_map
+
+    for folder_num in range(1, 6):
+
+        print( str( retrieve_individual_map(directory, min_req_base_sz_count, str(folder_num), file_name) ) + '\n\n' )
