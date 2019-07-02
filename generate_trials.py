a4a041b81d44215f1e6f1c8dc5ac36a8079c7842
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-07-02 09:41:47 -0400

    Update generate_trials.py

--- generate_trials.py
+++ generate_trials.py
@@ -308,7 +308,8 @@ def generate_one_trial_population(monthly_mean, monthly_std_dev, min_req_base_sz
     return [placebo_arm_daily_seizure_diaries, drug_arm_daily_seizure_diaries]
 
 
-def generate_one_iteration(monthly_mean, monthly_std_dev, min_req_base_sz_count, rct_constants_monthly_scale, effect_constants):
+def generate_one_iteration(monthly_mean, monthly_std_dev, min_req_base_sz_count, 
+                           rct_constants_monthly_scale, effect_constants):
     
     '''
 
