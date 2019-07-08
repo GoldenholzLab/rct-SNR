a278fd8bdbcdd3169d67c5e56e0e6a0b1f683628
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-07-08 16:23:34 -0400

    Update sum_generated_maps_wrapper.sh

--- sum_generated_maps_wrapper.sh
+++ sum_generated_maps_wrapper.sh
@@ -16,6 +16,8 @@ module load python/3.6.0
 source activate main_env
 
 directory='/n/scratch2/jmr95/test_maps_2'
+num_patients_per_model=10000
+num_months_per_patient=24
 shape_1=24.143
 scale_1=297.366
 alpha_1=284.024
@@ -26,13 +28,15 @@ alpha_2=296.339
 beta_2=243.719
 
 inputs[0]=$directory
-inputs[1]=$shape_1
-inputs[3]=$scale_1
-inputs[4]=$alpha_1
-inputs[5]=$beta_1
-inputs[6]=$shape_2
-inputs[7]=$scale_2
-inputs[8]=$alpha_2
-inputs[9]=$beta_2
+inputs[1]=$num_patients_per_model
+inputs[2]=$num_months_per_patient
+inputs[3]=$shape_1
+inputs[4]=$scale_1
+inputs[5]=$alpha_1
+inputs[6]=$beta_1
+inputs[7]=$shape_2
+inputs[8]=$scale_2
+inputs[9]=$alpha_2
+inputs[10]=$beta_2
 
 srun -c 1 python sum_generated_maps.py ${inputs[@]}
