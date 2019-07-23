737ae2a23c66f0b910c86edf85ec7bd052b19e48
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-07-23 16:46:42 -0400

    Update calculate_RMSE.py

--- calculate_RMSE.py
+++ calculate_RMSE.py
@@ -7,27 +7,28 @@ if (__name__=='__main__'):
 
     num_iter = int(sys.argv[1])
 
-    fisher_exact_emp_stat_power_err_sq_array = np.zeros(num_iter)
-    fisher_exact_ana_stat_power_err_sq_array = np.zeros(num_iter)
+    fisher_exact_emp_stat_power_err_sq_array = []
+    fisher_exact_ana_stat_power_err_sq_array = []
 
     for iter_num in range(1, num_iter + 1):
 
         json_filepath = os.getcwd() + '/' + str(iter_num) + '.json'
-        with open(json_filepath, 'r') as json_file:
-            data = json.load(json_file)
+        if ( os.path.isfile(json_filepath) ):
+            with open(json_filepath, 'r') as json_file:
+                data = json.load(json_file)
     
-        RR50_emp_stat_power         = data[0]
-        fisher_exact_emp_stat_power = data[1]
-        fisher_exact_ana_stat_power = data[2]
+            RR50_emp_stat_power         = data[0]
+            fisher_exact_emp_stat_power = data[1]
+            fisher_exact_ana_stat_power = data[2]
 
-        fisher_exact_emp_stat_power_err = RR50_emp_stat_power - fisher_exact_emp_stat_power
-        fisher_exact_ana_stat_power_err = RR50_emp_stat_power - fisher_exact_ana_stat_power
+            fisher_exact_emp_stat_power_err = RR50_emp_stat_power - fisher_exact_emp_stat_power
+            fisher_exact_ana_stat_power_err = RR50_emp_stat_power - fisher_exact_ana_stat_power
     
-        fisher_exact_emp_stat_power_err_sq = np.power(fisher_exact_emp_stat_power_err, 2)
-        fisher_exact_ana_stat_power_err_sq = np.power(fisher_exact_ana_stat_power_err, 2)
+            fisher_exact_emp_stat_power_err_sq = np.power(fisher_exact_emp_stat_power_err, 2)
+            fisher_exact_ana_stat_power_err_sq = np.power(fisher_exact_ana_stat_power_err, 2)
 
-        fisher_exact_emp_stat_power_err_sq_array[iter_num - 1] = fisher_exact_emp_stat_power_err_sq
-        fisher_exact_ana_stat_power_err_sq_array[iter_num - 1] = fisher_exact_ana_stat_power_err_sq
+            fisher_exact_emp_stat_power_err_sq_array.append(fisher_exact_emp_stat_power_err_sq)
+            fisher_exact_ana_stat_power_err_sq_array.append(fisher_exact_ana_stat_power_err_sq)
 
     fisher_exact_emp_stat_power_RMSE = np.sqrt(np.mean(fisher_exact_emp_stat_power_err_sq_array))
     fisher_exact_ana_stat_power_RMSE = np.sqrt(np.mean(fisher_exact_ana_stat_power_err_sq_array))
