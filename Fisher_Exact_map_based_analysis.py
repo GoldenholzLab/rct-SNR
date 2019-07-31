082da79565ab01f26c517a59292b23aaef6ea17d
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-07-31 11:30:00 -0400

    Update Fisher_Exact_map_based_analysis.py

--- Fisher_Exact_map_based_analysis.py
+++ Fisher_Exact_map_based_analysis.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as stats
 import json
 import os
 import subprocess
@@ -45,7 +46,7 @@ def generate_patient_pop_params(monthly_mean_min,
     return patient_pop_monthly_param_sets
 
 
-def calculate_expected_RR50_responses(num_theo_patients_per_trial_arm,
+def calculate_expected_RR50_responses_from_maps(num_theo_patients_per_trial_arm,
                                       placebo_arm_patient_pop_monthly_param_sets,
                                       drug_arm_patient_pop_monthly_param_sets,
                                       expected_RR50_placebo_response_map,
@@ -116,12 +117,12 @@ def generate_one_trial_arm_of_patient_diaries(patient_pop_monthly_param_sets,
     return daily_patient_diaries
 
 
-def calculate_individual_patient_percent_changes(daily_patient_diaries,
-                                                 num_patients_per_trial_arm, 
-                                                 num_days_per_patient_baseline):
+def calculate_individual_patient_percent_changes_per_diary_set(daily_patient_diaries,
+                                                               num_patients_per_trial_arm, 
+                                                               num_baseline_days_per_patient):
     
-    baseline_daily_seizure_diaries = daily_patient_diaries[:, :num_days_per_patient_baseline]
-    testing_daily_seizure_diaries  = daily_patient_diaries[:, num_days_per_patient_baseline:]
+    baseline_daily_seizure_diaries = daily_patient_diaries[:, :num_baseline_days_per_patient]
+    testing_daily_seizure_diaries  = daily_patient_diaries[:, num_baseline_days_per_patient:]
     
     baseline_daily_seizure_frequencies = np.mean(baseline_daily_seizure_diaries, 1)
     testing_daily_seizure_frequencies = np.mean(testing_daily_seizure_diaries, 1)
@@ -135,11 +136,14 @@ def calculate_individual_patient_percent_changes(daily_patient_diaries,
     return percent_changes
 
 
-def apply_effect(daily_patient_diaries,         num_patients_per_trial_arm, 
-                 num_days_per_patient_baseline, num_days_per_patient_testing, 
-                 effect_mu,                     effect_sigma):
+def apply_effect(daily_patient_diaries,
+                 num_patients_per_trial_arm, 
+                 num_baseline_days_per_patient,
+                 num_testing_days_per_patient, 
+                 effect_mu,
+                 effect_sigma):
 
-    testing_daily_patient_diaries = daily_patient_diaries[:, num_days_per_patient_baseline:]
+    testing_daily_patient_diaries = daily_patient_diaries[:,  num_baseline_days_per_patient:]
 
     for patient_index in range(num_patients_per_trial_arm):
 
@@ -149,7 +153,7 @@ def apply_effect(daily_patient_diaries,         num_patients_per_trial_arm,
 
         current_testing_daily_patient_diary = testing_daily_patient_diaries[patient_index, :]
 
-        for day_index in range(num_days_per_patient_testing):
+        for day_index in range(num_testing_days_per_patient):
 
             current_seizure_count = current_testing_daily_patient_diary[day_index]
             num_removed = 0
@@ -165,7 +169,7 @@ def apply_effect(daily_patient_diaries,         num_patients_per_trial_arm,
 
         testing_daily_patient_diaries[patient_index, :] = current_testing_daily_patient_diary
     
-    daily_patient_diaries[:, num_days_per_patient_baseline:] = testing_daily_patient_diaries
+    daily_patient_diaries[:, num_baseline_days_per_patient:] = testing_daily_patient_diaries
 
     return daily_patient_diaries
 
@@ -190,31 +194,132 @@ def generate_individual_patient_percent_changes_per_trial_arm(patient_pop_monthl
         
     one_trial_arm_daily_seizure_diaries = \
         apply_effect(one_trial_arm_daily_seizure_diaries,
+                     num_patients_per_trial_arm, 
                      num_baseline_days_per_patient,
                      num_testing_days_per_patient,
-                     num_patients_per_trial_arm,
                      placebo_mu,
                      placebo_sigma)
         
     one_trial_arm_daily_seizure_diaries = \
         apply_effect(one_trial_arm_daily_seizure_diaries,
+                     num_patients_per_trial_arm, 
                      num_baseline_days_per_patient,
                      num_testing_days_per_patient,
-                     num_patients_per_trial_arm,
                      drug_mu,
                      drug_sigma)
 
-    '''
-    [one_trial_arm_TTP_times, one_trial_arm_observed_array] = \
-        calculate_individual_patient_TTP_times_per_diary_set(one_trial_arm_daily_seizure_diaries,
-                                                             num_baseline_days_per_patient,
-                                                             num_testing_days_per_patient,
-                                                             num_theo_patients_per_trial_arm)
+    percent_changes = \
+        calculate_individual_patient_percent_changes_per_diary_set(one_trial_arm_daily_seizure_diaries,
+                                                                   num_patients_per_trial_arm, 
+                                                                   num_baseline_days_per_patient)
     
-    return [one_trial_arm_TTP_times, one_trial_arm_observed_array]
-    '''
+    return percent_changes
 
-'''
+
+def calculate_one_trial_p_value(placebo_arm_patient_pop_monthly_param_sets,
+                                drug_arm_patient_pop_monthly_param_sets,
+                                num_patients_per_trial_arm,
+                                num_baseline_days_per_patient,
+                                num_testing_days_per_patient,
+                                num_total_days_per_patient,
+                                min_req_base_sz_count,
+                                placebo_mu,
+                                placebo_sigma,
+                                drug_mu,
+                                drug_sigma):
+
+    placebo_arm_percent_changes = \
+        generate_individual_patient_percent_changes_per_trial_arm(placebo_arm_patient_pop_monthly_param_sets, 
+                                                                  num_patients_per_trial_arm,
+                                                                  num_baseline_days_per_patient,
+                                                                  num_testing_days_per_patient,
+                                                                  num_total_days_per_patient,
+                                                                  min_req_base_sz_count,
+                                                                  placebo_mu,
+                                                                  placebo_sigma,
+                                                                  0,
+                                                                  0)
+    
+    drug_arm_percent_changes = \
+        generate_individual_patient_percent_changes_per_trial_arm(placebo_arm_patient_pop_monthly_param_sets, 
+                                                                  num_patients_per_trial_arm,
+                                                                  num_baseline_days_per_patient,
+                                                                  num_testing_days_per_patient,
+                                                                  num_total_days_per_patient,
+                                                                  min_req_base_sz_count,
+                                                                  placebo_mu,
+                                                                  placebo_sigma,
+                                                                  drug_mu,
+                                                                  drug_sigma)
+    
+    num_placebo_responders     = np.sum(placebo_arm_percent_changes > 0.5)
+    num_placebo_non_responders = num_patients_per_trial_arm - num_placebo_responders
+    num_drug_responders        = np.sum(drug_arm_percent_changes > 0.5)
+    num_drug_non_responders    = num_patients_per_trial_arm - num_drug_responders
+    table = np.array([[num_placebo_responders, num_placebo_non_responders], [num_drug_responders,num_drug_non_responders]])
+    [_, p_value] = stats.fisher_exact(table)
+
+    return p_value
+
+
+def calculate_empirical_statistical_power(placebo_arm_patient_pop_monthly_param_sets,
+                                          drug_arm_patient_pop_monthly_param_sets,
+                                          num_patients_per_trial_arm,
+                                          num_baseline_days_per_patient,
+                                          num_testing_days_per_patient,
+                                          num_total_days_per_patient,
+                                          min_req_base_sz_count,
+                                          placebo_mu,
+                                          placebo_sigma,
+                                          drug_mu,
+                                          drug_sigma,
+                                          num_trials,
+                                          num_iter):
+
+    p_value_array = np.zeros(num_trials)
+
+    for trial_index in range(num_trials):
+
+        print('trial #' + str(trial_index + 1) + ', iter #' + str(num_iter + 1))
+
+        p_value_array[trial_index] = \
+            calculate_one_trial_p_value(placebo_arm_patient_pop_monthly_param_sets,
+                                        drug_arm_patient_pop_monthly_param_sets,
+                                        num_patients_per_trial_arm,
+                                        num_baseline_days_per_patient,
+                                        num_testing_days_per_patient,
+                                        num_total_days_per_patient,
+                                        min_req_base_sz_count,
+                                        placebo_mu,
+                                        placebo_sigma,
+                                        drug_mu,
+                                        drug_sigma)
+    
+    emp_stat_power = 100*np.sum(p_value_array < 0.05)/num_trials
+
+    return emp_stat_power
+
+
+def calculate_analytical_statistical_power(num_theo_patients_per_trial_arm,
+                                           placebo_arm_patient_pop_monthly_param_sets,
+                                           drug_arm_patient_pop_monthly_param_sets):
+
+    [expected_RR50_placebo_response_map, expected_RR50_drug_response_map] = get_RR50_response_maps()
+
+    [expected_RR50_placebo_response, expected_RR50_drug_response] = \
+        calculate_expected_RR50_responses_from_maps(num_theo_patients_per_trial_arm,
+                                                    placebo_arm_patient_pop_monthly_param_sets,
+                                                    drug_arm_patient_pop_monthly_param_sets,
+                                                    expected_RR50_placebo_response_map,
+                                                    expected_RR50_drug_response_map)
+
+    command = ['Rscript', 'Fisher_Exact_Power_Calc.R', str(expected_RR50_placebo_response), str(expected_RR50_drug_response), str(num_theo_patients_per_trial_arm), str(num_theo_patients_per_trial_arm)]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    fisher_exact_stat_power = 100*float(process.communicate()[0].decode().split()[1])
+
+    return fisher_exact_stat_power
+
+
 if(__name__=='__main__'):
 
     monthly_mean_min     = 4
@@ -222,34 +327,64 @@ if(__name__=='__main__'):
     monthly_std_dev_min  = 1
     monthly_std_dev_max  = 8
 
+    min_req_base_sz_count           = 4
+    num_baseline_months_per_patient = 2
+    num_testing_months_per_patient  = 3
     num_theo_patients_per_trial_arm = 153
 
-    [expected_RR50_placebo_response_map, expected_RR50_drug_response_map] = get_RR50_response_maps()
+    placebo_mu    = 0
+    placebo_sigma = 0.05
+    drug_mu       = 0.2
+    drug_sigma    = 0.05
 
-    placebo_arm_patient_pop_monthly_param_sets = \
-        generate_patient_pop_params(monthly_mean_min,
-                                    monthly_mean_max, 
-                                    monthly_std_dev_min, 
-                                    monthly_std_dev_max, 
-                                    num_theo_patients_per_trial_arm)
+    num_trials     = 500
+    num_iterations = 100
 
-    drug_arm_patient_pop_monthly_param_sets = \
-        generate_patient_pop_params(monthly_mean_min,
-                                    monthly_mean_max, 
-                                    monthly_std_dev_min, 
-                                    monthly_std_dev_max, 
-                                    num_theo_patients_per_trial_arm)
+    num_baseline_days_per_patient = num_baseline_months_per_patient*28
+    num_testing_days_per_patient  = num_testing_months_per_patient*28
+    num_total_days_per_patient = num_baseline_days_per_patient + num_testing_days_per_patient
 
-    [expected_RR50_placebo_response, expected_RR50_drug_response] = \
-        calculate_expected_RR50_responses(num_theo_patients_per_trial_arm,
-                                          placebo_arm_patient_pop_monthly_param_sets,
-                                          drug_arm_patient_pop_monthly_param_sets,
-                                          expected_RR50_placebo_response_map,
-                                          expected_RR50_drug_response_map)
+    fisher_exact_stat_power_array = np.zeros(num_iterations)
+    emp_stat_power_array          = np.zeros(num_iterations)
 
-    command = ['Rscript', 'Fisher_Exact_Power_Calc.R', str(expected_RR50_placebo_response), str(expected_RR50_drug_response), str(num_theo_patients_per_trial_arm), str(num_theo_patients_per_trial_arm)]
-    process = subprocess.Popen(command, stdout=subprocess.PIPE)
-    fisher_exact_stat_power = 100*float(process.communicate()[0].decode().split()[1])
+    for num_iter in range(num_iterations):
 
-    print(fisher_exact_stat_power)
-'''
+        placebo_arm_patient_pop_monthly_param_sets = \
+            generate_patient_pop_params(monthly_mean_min,
+                                        monthly_mean_max, 
+                                        monthly_std_dev_min, 
+                                        monthly_std_dev_max, 
+                                        num_theo_patients_per_trial_arm)
+
+        drug_arm_patient_pop_monthly_param_sets = \
+            generate_patient_pop_params(monthly_mean_min,
+                                        monthly_mean_max, 
+                                        monthly_std_dev_min, 
+                                        monthly_std_dev_max, 
+                                        num_theo_patients_per_trial_arm)
+
+        fisher_exact_stat_power = \
+            calculate_analytical_statistical_power(num_theo_patients_per_trial_arm,
+                                                   placebo_arm_patient_pop_monthly_param_sets,
+                                                   drug_arm_patient_pop_monthly_param_sets)
+
+        emp_stat_power = \
+            calculate_empirical_statistical_power(placebo_arm_patient_pop_monthly_param_sets,
+                                                  drug_arm_patient_pop_monthly_param_sets,
+                                                  num_theo_patients_per_trial_arm,
+                                                  num_baseline_days_per_patient,
+                                                  num_testing_days_per_patient,
+                                                  num_total_days_per_patient,
+                                                  min_req_base_sz_count,
+                                                  placebo_mu,
+                                                  placebo_sigma,
+                                                  drug_mu,
+                                                  drug_sigma,
+                                                  num_trials,
+                                                  num_iter)
+
+        fisher_exact_stat_power_array[num_iter] = fisher_exact_stat_power
+        emp_stat_power_array[num_iter]          = emp_stat_power
+    
+    
+
