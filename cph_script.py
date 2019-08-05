891e4076e031a911d320fe960ff54c30c28d3aeb
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-08-05 14:51:13 -0400

    Update cph_script.py

--- cph_script.py
+++ cph_script.py
@@ -1,7 +1,9 @@
 import numpy as np
+from lifelines.statistics import logrank_test
 import pandas as pd
 import subprocess
 import os
+import time
 
 
 def generate_patient_pop_params(monthly_mean_min,
@@ -29,6 +31,282 @@ def generate_patient_pop_params(monthly_mean_min,
         patient_pop_monthly_param_sets[theo_patient_index, 1] = monthly_std_dev
 
     return patient_pop_monthly_param_sets
+
+
+def generate_one_trial_arm_of_patient_diaries(patient_pop_monthly_param_sets, 
+                                              num_patients_per_trial_arm, 
+                                              min_req_base_sz_count,
+                                              num_baseline_days_per_patient, 
+                                              num_total_days_per_patient):
+
+    daily_patient_diaries = np.zeros((num_patients_per_trial_arm, num_total_days_per_patient))
+
+    for patient_index in range(num_patients_per_trial_arm):
+
+        monthly_mean    = patient_pop_monthly_param_sets[patient_index, 0]
+        monthly_std_dev = patient_pop_monthly_param_sets[patient_index, 1]
+
+        daily_mean = monthly_mean/28
+        daily_std_dev = monthly_std_dev/np.sqrt(28)
+        daily_var = np.power(daily_std_dev, 2)
+        daily_overdispersion = (daily_var - daily_mean)/np.power(daily_mean, 2)
+
+        daily_n = 1/daily_overdispersion
+        daily_odds_ratio = daily_overdispersion*daily_mean
+
+        acceptable_baseline = False
+        current_daily_patient_diary = np.zeros(num_total_days_per_patient)
+
+        while(not acceptable_baseline):
+
+            for day_index in range(num_total_days_per_patient):
+
+                daily_rate = np.random.gamma(daily_n, daily_odds_ratio)
+                daily_count = np.random.poisson(daily_rate)
+
+                current_daily_patient_diary[day_index] = daily_count
+                
+            current_patient_baseline_sz_count = np.sum(current_daily_patient_diary[:num_baseline_days_per_patient])
+                
+            if(current_patient_baseline_sz_count >= min_req_base_sz_count):
+
+                    acceptable_baseline = True
+            
+        daily_patient_diaries[patient_index, :] = current_daily_patient_diary
+    
+    return daily_patient_diaries
+
+
+def calculate_individual_patient_TTP_times_per_diary_set(daily_seizure_diaries,
+                                                         num_baseline_days_per_patient,
+                                                         num_testing_days_per_patient,
+                                                         num_daily_seizure_diaries):
+
+    baseline_daily_seizure_diaries = daily_seizure_diaries[:, :num_baseline_days_per_patient]
+    testing_daily_seizure_diaries  = daily_seizure_diaries[:, num_baseline_days_per_patient:]
+
+    baseline_monthly_seizure_frequencies = 28*np.mean(baseline_daily_seizure_diaries, 1)
+
+    TTP_times      = np.zeros(num_daily_seizure_diaries)
+    observed_array = np.zeros(num_daily_seizure_diaries)
+
+    for daily_seizure_diary_index in range(num_daily_seizure_diaries):
+
+        reached_count = False
+        day_index = 0
+        sum_count = 0
+
+        while(not reached_count):
+
+            sum_count = sum_count + testing_daily_seizure_diaries[daily_seizure_diary_index, day_index]
+
+            reached_count  = sum_count >= baseline_monthly_seizure_frequencies[daily_seizure_diary_index]
+            right_censored = day_index == (num_testing_days_per_patient - 1)
+            reached_count  = reached_count or right_censored
+
+            day_index = day_index + 1
+        
+        TTP_times[daily_seizure_diary_index]      = day_index
+        observed_array[daily_seizure_diary_index] = not right_censored
+    
+    return [TTP_times, observed_array]
+
+
+def apply_effect(daily_seizure_diaries,
+                 num_baseline_days_per_patient,
+                 num_testing_days_per_patient,
+                 num_seizure_diaries,
+                 effect_mu,
+                 effect_sigma):
+    
+    testing_daily_seizure_diaries  = daily_seizure_diaries[:, num_baseline_days_per_patient:]
+
+    for seizure_diary_index in range(num_seizure_diaries):
+
+        effect = np.random.normal(effect_mu, effect_sigma)
+        if(effect > 1):
+            effect = 1
+        
+        current_testing_daily_seizure_diary = testing_daily_seizure_diaries[seizure_diary_index, :]
+
+        for day_index in range(num_testing_days_per_patient):
+
+            current_seizure_count = current_testing_daily_seizure_diary[day_index]
+            num_removed = 0
+
+            for seizure_index in range(np.int_(current_seizure_count)):
+
+                if(np.random.random() <= np.abs(effect)):
+
+                    num_removed = num_removed + np.sign(effect)
+                
+            current_seizure_count = current_seizure_count - num_removed
+            current_testing_daily_seizure_diary[day_index] = current_seizure_count
+        
+        testing_daily_seizure_diaries[seizure_diary_index, :] = current_testing_daily_seizure_diary
+
+    daily_seizure_diaries[:, num_baseline_days_per_patient:] = testing_daily_seizure_diaries
+
+    return daily_seizure_diaries
+
+
+def generate_individual_patient_TTP_times_per_trial_arm(patient_pop_daily_params, 
+                                                        num_theo_patients_per_trial_arm,
+                                                        num_baseline_days_per_patient,
+                                                        num_testing_days_per_patient,
+                                                        num_total_days_per_patient,
+                                                        min_req_bs_sz_count,
+                                                        placebo_mu,
+                                                        placebo_sigma,
+                                                        drug_mu,
+                                                        drug_sigma):
+
+    one_trial_arm_daily_seizure_diaries = \
+        generate_one_trial_arm_of_seizure_diaries(patient_pop_daily_params, 
+                                                  num_theo_patients_per_trial_arm,
+                                                  num_baseline_days_per_patient,
+                                                  num_total_days_per_patient,
+                                                  min_req_bs_sz_count)
+        
+    one_trial_arm_daily_seizure_diaries = \
+        apply_effect(one_trial_arm_daily_seizure_diaries,
+                     num_baseline_days_per_patient,
+                     num_testing_days_per_patient,
+                     num_theo_patients_per_trial_arm,
+                     placebo_mu,
+                     placebo_sigma)
+        
+    one_trial_arm_daily_seizure_diaries = \
+        apply_effect(one_trial_arm_daily_seizure_diaries,
+                     num_baseline_days_per_patient,
+                     num_testing_days_per_patient,
+                     num_theo_patients_per_trial_arm,
+                     drug_mu,
+                     drug_sigma)
+
+    [one_trial_arm_TTP_times, one_trial_arm_observed_array] = \
+        calculate_individual_patient_TTP_times_per_diary_set(one_trial_arm_daily_seizure_diaries,
+                                                             num_baseline_days_per_patient,
+                                                             num_testing_days_per_patient,
+                                                             num_theo_patients_per_trial_arm)
+
+    return [one_trial_arm_TTP_times, one_trial_arm_observed_array]
+
+
+def generate_one_trial_TTP_times(patient_placebo_pop_daily_params, 
+                                 patient_drug_pop_daily_params,
+                                 min_req_bs_sz_count,
+                                 num_baseline_days_per_patient,
+                                 num_testing_days_per_patient,
+                                 num_total_days_per_patient,
+                                 num_theo_patients_per_trial_arm,
+                                 placebo_mu,
+                                 placebo_sigma,
+                                 drug_mu,
+                                 drug_sigma):
+
+    [one_placebo_arm_TTP_times, one_placebo_arm_observed_array] = \
+        generate_individual_patient_TTP_times_per_trial_arm(patient_placebo_pop_daily_params, 
+                                                            num_theo_patients_per_trial_arm,
+                                                            num_baseline_days_per_patient,
+                                                            num_testing_days_per_patient,
+                                                            num_total_days_per_patient,
+                                                            min_req_bs_sz_count,
+                                                            placebo_mu,
+                                                            placebo_sigma,
+                                                            0,
+                                                            0)
+    
+    [one_drug_arm_TTP_times, one_drug_arm_observed_array] = \
+        generate_individual_patient_TTP_times_per_trial_arm(patient_drug_pop_daily_params, 
+                                                            num_theo_patients_per_trial_arm,
+                                                            num_baseline_days_per_patient,
+                                                            num_testing_days_per_patient,
+                                                            num_total_days_per_patient,
+                                                            min_req_bs_sz_count,
+                                                            placebo_mu,
+                                                            placebo_sigma,
+                                                            drug_mu,
+                                                            drug_sigma)
+    
+    return [one_placebo_arm_TTP_times, one_placebo_arm_observed_array, 
+            one_drug_arm_TTP_times,    one_drug_arm_observed_array, ]
+
+
+def calculate_one_trial_p_value(placebo_arm_patient_pop_monthly_param_sets,
+                                drug_arm_patient_pop_monthly_param_sets,
+                                num_patients_per_trial_arm,
+                                num_baseline_days_per_patient,
+                                num_testing_days_per_patient,
+                                num_total_days_per_patient,
+                                min_req_base_sz_count,
+                                placebo_mu,
+                                placebo_sigma,
+                                drug_mu,
+                                drug_sigma):
+
+    [one_placebo_arm_TTP_times, one_placebo_arm_observed_array, 
+     one_drug_arm_TTP_times,    one_drug_arm_observed_array, ] = \
+        generate_one_trial_TTP_times(patient_placebo_pop_daily_params, 
+                                     patient_drug_pop_daily_params,
+                                     min_req_bs_sz_count,
+                                     num_baseline_days_per_patient,
+                                     num_testing_days_per_patient,
+                                     num_total_days_per_patient,
+                                     num_theo_patients_per_trial_arm,
+                                     placebo_mu,
+                                     placebo_sigma,
+                                     drug_mu,
+                                     drug_sigma)
+
+    TTP_results = logrank_test(one_placebo_arm_TTP_times,
+                               one_drug_arm_TTP_times, 
+                               one_placebo_arm_observed_array, 
+                               one_drug_arm_observed_array)
+    TTP_p_value = TTP_results.p_value
+
+    return TTP_p_value
+
+
+def calculate_empirical_statistical_power(placebo_arm_patient_pop_monthly_param_sets,
+                                          drug_arm_patient_pop_monthly_param_sets,
+                                          num_patients_per_trial_arm,
+                                          num_baseline_days_per_patient,
+                                          num_testing_days_per_patient,
+                                          num_total_days_per_patient,
+                                          min_req_base_sz_count,
+                                          placebo_mu,
+                                          placebo_sigma,
+                                          drug_mu,
+                                          drug_sigma,
+                                          num_trials):
+
+    p_value_array = np.zeros(num_trials)
+
+    for trial_index in range(num_trials):
+
+        trial_start_time_in_seconds = time.time()
+
+        p_value_array[trial_index] = \
+            calculate_one_trial_p_value(placebo_arm_patient_pop_monthly_param_sets,
+                                        drug_arm_patient_pop_monthly_param_sets,
+                                        num_patients_per_trial_arm,
+                                        num_baseline_days_per_patient,
+                                        num_testing_days_per_patient,
+                                        num_total_days_per_patient,
+                                        min_req_base_sz_count,
+                                        placebo_mu,
+                                        placebo_sigma,
+                                        drug_mu,
+                                        drug_sigma)
+        
+        trial_stop_time_in_seconds = time.time()
+        trial_runtime_str = str(np.round((trial_stop_time_in_seconds - trial_start_time_in_seconds)/60, 3))
+        print( 'trial #' + str(trial_index + 1) + ', runtime: ' + trial_runtime_str + ' minutes' )
+    
+    emp_stat_power = 100*np.sum(p_value_array < 0.05)/num_trials
+
+    return emp_stat_power
 
 
 def estimate_analytical_power(placebo_TTP_times,
