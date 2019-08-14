5510fd67e89a0403de55bd4eb4c0bdc7d34ead1b
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-08-14 14:04:37 -0400

    changed bash scripts to be submittable to O2

--- TTP Stuff/calculate_RMSE_power_TTP_power_wrapper.sh
+++ TTP Stuff/calculate_RMSE_power_TTP_power_wrapper.sh
@@ -1,7 +1,23 @@
+#!/usr/bin/bash
+
+#SBATCH -p short
+#SBATCH --mem=10G
+#SBATCH -t 0-00:10
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -e jmr95_%j.err
+#SBATCH -o jmr95_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jromero5@bidmc.harvard.edu
+
+module load gcc/6.2.0
+module load conda2/4.2.13
+module load python/3.6.0
+source activate main_env
 
 num_stat_power_estimates=1000
 bins=100
 #folder='/Users/juanromero/Documents/Python_3_Files/useless_folder'
 folder='/n/scratch2/jmr95/TTP_stat_power_estimates'
 
-python -u calculate_RMSE_TTP_power.py $num_stat_power_estimates $bins $folder
+srun -c 1 python -u calculate_RMSE_TTP_power.py $num_stat_power_estimates $bins $folder
--- TTP Stuff/cph_power_calc_investigation_submitter.sh
+++ TTP Stuff/cph_power_calc_investigation_submitter.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/bash
+
+#SBATCH -p short
+#SBATCH -t 0-00:05
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -e jmr95_%j.err
+#SBATCH -o jmr95_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
 monthly_mean_min=1
 monthly_mean_max=16
@@ -14,7 +24,7 @@ placebo_sigma=0.05
 drug_mu=0.2
 drug_sigma=0.05
 
-num_trials=100
+num_trials=2000
 alpha=0.05
 #folder='/Users/juanromero/Documents/Python_3_Files/useless_folder'
 folder='/n/scratch2/jmr95/TTP_stat_power_estimates'
@@ -40,6 +50,6 @@ for ((index=1; index<$num_stat_power_estimates+1; index=index+1))
 do
     inputs[15]=$index
 
-    bash cph_power_calc_investigation_wrapper.sh ${inputs[@]}
+    sbatch cph_power_calc_investigation_wrapper.sh ${inputs[@]}
 
 done
