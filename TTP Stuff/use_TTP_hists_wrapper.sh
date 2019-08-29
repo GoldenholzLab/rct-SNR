4b16c273db3d63cd71620daadf19350588b279bf
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-08-29 14:13:15 -0400

    Update use_TTP_hists_wrapper.sh

---
+++ TTP Stuff/use_TTP_hists_wrapper.sh
@@ -0,0 +1,25 @@
+
+
+monthly_mean_min=4
+monthly_mean_max=16
+monthly_std_dev_min=1
+monthly_std_dev_max=8
+
+num_baseline_months_per_patient = 2
+num_testing_months_per_patient  = 3
+min_req_base_sz_count           = 4
+
+placebo_mu    = 0
+placebo_sigma = 0.05
+drug_mu       = 0.2
+drug_sigma    = 0.05
+
+num_trials                      = 100
+num_theo_patients_per_trial_arm = 153
+num_patients_per_dot            = 5000
+alpha                           = 0.05
+
+hist_maps_folder          = os.getcwd() + '/hist_maps_folder'
+stat_power_storage_folder = '/Users/juanromero/Documents/Python_3_Files/useless_folder'
+stat_power_estimate_index = 0
+
