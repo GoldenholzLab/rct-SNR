f156e8eb468d5b2115b0d1f4a5afe046aa8406ab
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-08-29 16:04:35 -0400

    Update use_TTP_hists_wrapper.sh

--- TTP Stuff/use_TTP_hists_wrapper.sh
+++ TTP Stuff/use_TTP_hists_wrapper.sh
@@ -5,21 +5,43 @@ monthly_mean_max=16
 monthly_std_dev_min=1
 monthly_std_dev_max=8
 
-num_baseline_months_per_patient = 2
-num_testing_months_per_patient  = 3
-min_req_base_sz_count           = 4
+num_baseline_months_per_patient=2
+num_testing_months_per_patient=3
+min_req_base_sz_count=4
 
-placebo_mu    = 0
-placebo_sigma = 0.05
-drug_mu       = 0.2
-drug_sigma    = 0.05
+placebo_mu=0
+placebo_sigma=0.05
+drug_mu=0.2
+drug_sigma=0.05
 
-num_trials                      = 100
-num_theo_patients_per_trial_arm = 153
-num_patients_per_dot            = 5000
-alpha                           = 0.05
+num_trials=10
+num_theo_patients_per_trial_arm=15
+num_patients_per_dot=5000
+alpha=0.05
 
-hist_maps_folder          = os.getcwd() + '/hist_maps_folder'
-stat_power_storage_folder = '/Users/juanromero/Documents/Python_3_Files/useless_folder'
-stat_power_estimate_index = 0
+hist_maps_folder="$(pwd)/hist_maps_folder"
+stat_power_storage_folder='/Users/juanromero/Documents/Python_3_Files/useless_folder'
+num_stat_power_estimates=10
 
+
+inputs[0]=$monthly_mean_min
+inputs[1]=$monthly_mean_max
+inputs[2]=$monthly_std_dev_min
+inputs[3]=$monthly_std_dev_max
+inputs[4]=$num_baseline_months_per_patient
+inputs[5]=$num_testing_months_per_patient
+inputs[6]=$min_req_base_sz_count
+inputs[7]=$placebo_mu
+inputs[8]=$placebo_sigma
+inputs[9]=$drug_mu
+inputs[10]=$drug_sigma
+inputs[11]=$hist_maps_folder
+inputs[12]=$stat_power_storage_folder
+
+for ((stat_power_estimate_index=1; stat_power_estimate_index<=$num_stat_power_estimates ; stat_power_estimate_index=stat_power_estimate_index+1))
+do
+    inputs[13]=$stat_power_estimate_index
+
+    python -u use_TTP_hists.py ${inputs[@]} 
+
+done
