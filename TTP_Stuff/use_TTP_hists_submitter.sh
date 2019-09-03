666c393b41e8dad9fe149bb258b8ef860856a4ef
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-09-03 11:55:27 -0400

    Update use_TTP_hists_submitter.sh

--- TTP_Stuff/use_TTP_hists_submitter.sh
+++ TTP_Stuff/use_TTP_hists_submitter.sh
@@ -30,7 +30,7 @@ alpha=0.05
 
 #hist_maps_folder="$(pwd)/hist_maps_folder"
 #stat_power_storage_folder='/Users/juanromero/Documents/Python_3_Files/useless_folder'
-hist_maps_folder="$/n/scratch2/jmr95/rct-SNR-9_3_2019/hist_maps_folder"
+hist_maps_folder="/n/scratch2/jmr95/rct-SNR-9_3_2019/hist_maps_folder"
 stat_power_storage_folder='/n/scratch2/jmr95/rct-SNR-9_3_2019/TTP_stat_power_estimates'
 num_stat_power_estimates=10
 
