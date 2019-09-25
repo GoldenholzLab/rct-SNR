15e58d30eefa5b168e435f609b041c9f3885ff87
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-09-25 17:00:08 -0400

    Create TTP_stat_power_prediction.py

---
+++ python_and_R_scripts/TTP_stat_power_prediction.py
@@ -0,0 +1,209 @@
+import numpy as np
+from patient_population_generation import generate_theo_patient_pop_params
+from empirical_estimation import empirically_estimate_TTP_statistical_power
+import json
+import pandas as pd
+import os
+import subprocess
+
+
+def retrieve_map_location_data(folder, 
+                               monthly_mean, 
+                               monthly_std_dev, 
+                               trial_arm):
+
+    if((trial_arm == 'placebo_arm') or (trial_arm == 'drug_arm')):
+
+        file_name_prefix = 'mean_' + str(np.int_(monthly_mean)) + '_std_' + str(np.int_(monthly_std_dev)) + '_' + trial_arm
+
+        TTP_times_file_name        = folder + '/' + file_name_prefix + '_TTP_times.json'
+        TTP_observations_file_name = folder + '/' + file_name_prefix + '_TTP_observations.json'
+
+        with open(TTP_times_file_name, 'r') as json_file:
+
+            TTP_times = np.array(json.load(json_file))
+    
+        with open(TTP_observations_file_name, 'r') as json_file:
+
+            TTP_observations = np.array(json.load(json_file))
+    
+        return [TTP_times, TTP_observations]
+    
+    else:
+
+        raise ValueError('The \'trial_arm\' variable must either be \'placebo_arm\' or \'drug_arm\'')
+
+
+def collect_population_data(folder,
+                            trial_arm,
+                            num_theo_patients_per_trial_arm,
+                            theo_patient_pop_params):
+
+    trial_arm_TTP_times        = np.array([])
+    trial_arm_TTP_observations = np.array([])
+
+    monthly_mean_array    = theo_patient_pop_params[:, 0]
+    monthly_std_dev_array = theo_patient_pop_params[:, 1]
+
+    for patient_index in range(num_theo_patients_per_trial_arm):
+
+        monthly_mean    = monthly_mean_array[patient_index]
+        monthly_std_dev = monthly_std_dev_array[patient_index]
+
+        [tmp_trial_arm_TTP_times, 
+         tmp_trial_arm_TTP_observations] = \
+             retrieve_map_location_data(folder, 
+                                        monthly_mean, 
+                                        monthly_std_dev, 
+                                        trial_arm)
+        
+        trial_arm_TTP_times        = np.concatenate([trial_arm_TTP_times,        tmp_trial_arm_TTP_times])
+        trial_arm_TTP_observations = np.concatenate([trial_arm_TTP_observations, tmp_trial_arm_TTP_observations])
+    
+    return [trial_arm_TTP_times, trial_arm_TTP_observations]
+
+
+def calculate_one_theo_pop_analytical_quantities(placebo_arm_TTP_times,
+                                                 placebo_arm_TTP_observations,
+                                                 drug_arm_TTP_times,
+                                                 drug_arm_TTP_observations,
+                                                 num_theo_patients_per_trial_arm,
+                                                 num_patients_per_map_location,
+                                                 tmp_file_name):
+
+    relative_tmp_file_path = tmp_file_name + '.csv'
+    TTP_times              = np.append(placebo_arm_TTP_times, drug_arm_TTP_times)
+    events                 = np.append(placebo_arm_TTP_observations, drug_arm_TTP_observations)
+    treatment_arms_str     = np.append( np.array(num_theo_patients_per_trial_arm*num_patients_per_map_location*['C']), np.array(num_theo_patients_per_trial_arm*num_patients_per_map_location*['E']) )
+    treatment_arms         = np.int_(treatment_arms_str == "C")
+
+    data = np.array([TTP_times, events, treatment_arms, treatment_arms_str]).transpose()
+    pd.DataFrame(data, columns=['TTP_times', 'events', 'treatment_arms', 'treatment_arms_str']).to_csv(relative_tmp_file_path)
+    command = ['Rscript', 'python_and_R_scripts/estimate_log_hazard_ratio.R', relative_tmp_file_path]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    postulated_log_hazard_ratio = float(process.communicate()[0].decode().split()[1])
+
+    os.remove(relative_tmp_file_path)
+
+    prob_fail_placebo_arm = np.sum(placebo_arm_TTP_observations == True)/(num_theo_patients_per_trial_arm*num_patients_per_map_location)
+    prob_fail_drug_arm    = np.sum(drug_arm_TTP_observations == True)/(num_theo_patients_per_trial_arm*num_patients_per_map_location)
+
+    return [postulated_log_hazard_ratio, prob_fail_placebo_arm, prob_fail_drug_arm]
+
+
+def estimate_map_based_statistical_power(num_theo_patients_per_trial_arm,
+                                         prob_fail_drug_arm,
+                                         prob_fail_placebo_arm,
+                                         postulated_log_hazard_ratio,
+                                         alpha):
+
+    command        = ['Rscript', 'python_and_R_scripts/calculate_cph_power.R', str(num_theo_patients_per_trial_arm), str(num_theo_patients_per_trial_arm), 
+                      str(prob_fail_drug_arm), str(prob_fail_placebo_arm), str(np.exp(postulated_log_hazard_ratio)), str(alpha)]
+    process        = subprocess.Popen(command, stdout=subprocess.PIPE)
+    map_stat_power = 100*float(process.communicate()[0].decode().split()[1])
+
+    return map_stat_power
+
+
+def estimate_map_based_stat_power_for_one_pop(folder,
+                                              num_theo_patients_per_trial_arm,
+                                              num_patients_per_map_location,
+                                              tmp_file_name,
+                                              alpha,
+                                              theo_placebo_arm_patient_pop_params,
+                                              theo_drug_arm_patient_pop_params):
+
+    [placebo_arm_TTP_times, 
+     placebo_arm_TTP_observations] = \
+         collect_population_data(folder,
+                                 'placebo_arm',
+                                 num_theo_patients_per_trial_arm,
+                                 theo_placebo_arm_patient_pop_params)
+    
+    [drug_arm_TTP_times, 
+     drug_arm_TTP_observations] = \
+         collect_population_data(folder,
+                                 'drug_arm',
+                                 num_theo_patients_per_trial_arm,
+                                 theo_drug_arm_patient_pop_params)
+
+    [postulated_log_hazard_ratio, 
+     prob_fail_placebo_arm, 
+     prob_fail_drug_arm] = \
+        calculate_one_theo_pop_analytical_quantities(placebo_arm_TTP_times,
+                                                     placebo_arm_TTP_observations,
+                                                     drug_arm_TTP_times,
+                                                     drug_arm_TTP_observations,
+                                                     num_theo_patients_per_trial_arm,
+                                                     num_patients_per_map_location,
+                                                     tmp_file_name)
+    
+    map_stat_power = \
+        estimate_map_based_statistical_power(num_theo_patients_per_trial_arm,
+                                             prob_fail_drug_arm,
+                                             prob_fail_placebo_arm,
+                                             postulated_log_hazard_ratio,
+                                             alpha)
+    
+    return map_stat_power
+
+
+if(__name__=='__main__'):
+
+    monthly_mean_min = 1
+    monthly_mean_max = 16
+    monthly_std_dev_min = 1
+    monthly_std_dev_max = 16
+    num_theo_patients_per_trial_arm = 153
+    
+    folder = os.getcwd() + '/test_folder'
+    num_patients_per_map_location = 5000
+    tmp_file_name = 'test'
+    alpha = 0.05
+
+    num_baseline_months = 2
+    num_testing_months = 3
+    minimum_required_baseline_seizure_count = 4
+    placebo_mu = 0
+    placebo_sigma = 0.05 
+    drug_mu = 0.2
+    drug_sigma = 0.05
+    num_trials = 5000
+
+    theo_placebo_arm_patient_pop_params = \
+        generate_theo_patient_pop_params(monthly_mean_min,
+                                         monthly_mean_max,
+                                         monthly_std_dev_min,
+                                         monthly_std_dev_max,
+                                         num_theo_patients_per_trial_arm)
+    
+    theo_drug_arm_patient_pop_params = \
+        generate_theo_patient_pop_params(monthly_mean_min,
+                                         monthly_mean_max,
+                                         monthly_std_dev_min,
+                                         monthly_std_dev_max,
+                                         num_theo_patients_per_trial_arm)
+    
+    TTP_emp_stat_power = \
+        empirically_estimate_TTP_statistical_power(theo_placebo_arm_patient_pop_params,
+                                                   theo_drug_arm_patient_pop_params,
+                                                   num_theo_patients_per_trial_arm,
+                                                   num_baseline_months,
+                                                   num_testing_months,
+                                                   minimum_required_baseline_seizure_count,
+                                                   placebo_mu,
+                                                   placebo_sigma,
+                                                   drug_mu,
+                                                   drug_sigma,
+                                                   num_trials)
+
+    map_stat_power = \
+        estimate_map_based_stat_power_for_one_pop(folder,
+                                                  num_theo_patients_per_trial_arm,
+                                                  num_patients_per_map_location,
+                                                  tmp_file_name,
+                                                  alpha,
+                                                  theo_placebo_arm_patient_pop_params,
+                                                  theo_drug_arm_patient_pop_params)
+    
+    print([map_stat_power, TTP_emp_stat_power])
