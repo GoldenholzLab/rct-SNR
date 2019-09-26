b091865abce82ede7c836a696495b865c096b054
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-09-26 16:17:22 -0400

    Update TTP_emp_and_map_power_estimation_submitter.sh
    got rid of unnecesary module loading

--- TTP_emp_and_map_power_estimation_submitter.sh
+++ TTP_emp_and_map_power_estimation_submitter.sh
@@ -9,10 +9,6 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
-module load gcc/6.2.0
-module load conda2/4.2.13
-module load python/3.6.0
-source activate main_env
 monthly_mean_min=4
 monthly_mean_max=16
 monthly_std_dev_min=1
