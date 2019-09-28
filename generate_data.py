9664436f88347ca29733d87913815ea1b262183d
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-09-28 10:47:01 -0400

    made code implementable on O2

--- generate_data.py
+++ generate_data.py
@@ -3,6 +3,9 @@ from utility_code.patient_population_generation import convert_theo_pop_hist
 from utility_code.empirical_estimation import empirically_estimate_RR50_statistical_power
 import time
 import numpy as np
+import json
+import os
+import sys
 
 
 def generate_theo_trial_arm_patient_pops_and_hist(monthly_mean_min,
@@ -112,41 +115,104 @@ def generate_theo_pop_hists_and_power(monthly_mean_min,
     return [theo_placebo_arm_pop_hist, theo_drug_arm_pop_hist, RR50_emp_stat_power]
 
 
+def store_theo_pop_hists_and_emp_stat_powers(theo_placebo_arm_hists,
+                                             theo_drug_arm_hists,
+                                             RR50_emp_stat_powers,
+                                             data_storage_folder_name,
+                                             compute_iter):
+    
+    data_storage_folder_file_path = './' + data_storage_folder_name
+
+    if( not os.path.isdir(data_storage_folder_file_path) ):
+        os.makedirs(data_storage_folder_file_path)
+
+    theo_placebo_arm_hists_file_name = 'theo_placebo_arm_hists_' + str(compute_iter) + '.json'
+    theo_drug_arm_hists_file_name    = 'theo_drug_arm_hists_'    + str(compute_iter) + '.json'
+    RR50_emp_stat_powers_file_name   = 'RR50_emp_stat_powers_'   + str(compute_iter) + '.json'
+    
+    theo_placebo_arm_hists_file_path = data_storage_folder_file_path + '/' + theo_placebo_arm_hists_file_name
+    theo_drug_arm_hists_file_path    = data_storage_folder_file_path + '/' + theo_drug_arm_hists_file_name
+    RR50_emp_stat_powers_file_path   = data_storage_folder_file_path + '/' + RR50_emp_stat_powers_file_name
+    
+    with open(theo_placebo_arm_hists_file_path, 'w+') as json_file:
+        json.dump(theo_placebo_arm_hists.tolist(), json_file)
+    
+    with open(theo_drug_arm_hists_file_path, 'w+') as json_file:
+        json.dump(theo_drug_arm_hists.tolist(), json_file)
+    
+    with open(RR50_emp_stat_powers_file_path, 'w+') as json_file:
+        json.dump(RR50_emp_stat_powers.tolist(), json_file)
+
+
+def take_inputs_from_command_shell():
+
+    monthly_mean_min    = int(sys.argv[1])
+    monthly_mean_max    = int(sys.argv[2])
+    monthly_std_dev_min = int(sys.argv[3])
+    monthly_std_dev_max = int(sys.argv[4])
+
+    num_theo_patients_per_trial_arm = int(sys.argv[5])
+    num_baseline_months = int(sys.argv[6])
+    num_testing_months  = int(sys.argv[7])
+    minimum_required_baseline_seizure_count = int(sys.argv[8])
+
+    placebo_mu    = float(sys.argv[9])
+    placebo_sigma = float(sys.argv[10])
+    drug_mu       = float(sys.argv[11])
+    drug_sigma    = float(sys.argv[12])
+
+    num_trials      = int(sys.argv[13])
+    num_pops        = int(sys.argv[14])
+    data_storage_folder = sys.argv[15]
+    compute_iter    = int(sys.argv[16])
+
+    return [monthly_mean_min, monthly_mean_max, monthly_std_dev_min, monthly_std_dev_max,
+            num_theo_patients_per_trial_arm, num_baseline_months, num_testing_months, 
+            minimum_required_baseline_seizure_count,
+            placebo_mu, placebo_sigma, drug_mu, drug_sigma,
+            num_trials, num_pops, data_storage_folder, compute_iter]
+
+
 if(__name__=='__main__'):
 
-    monthly_mean_min    = 4
-    monthly_mean_max    = 16
-    monthly_std_dev_min = 1
-    monthly_std_dev_max = 8
+    [monthly_mean_min, monthly_mean_max, monthly_std_dev_min, monthly_std_dev_max,
+     num_theo_patients_per_trial_arm, num_baseline_months, num_testing_months, 
+     minimum_required_baseline_seizure_count,
+     placebo_mu, placebo_sigma, drug_mu, drug_sigma,
+     num_trials, num_pops, data_storage_folder, compute_iter] = take_inputs_from_command_shell()
 
-    num_theo_patients_per_trial_arm = 153
-    num_baseline_months = 2
-    num_testing_months  = 3
-    minimum_required_baseline_seizure_count = 4
+    num_monthly_means    = monthly_mean_max - (monthly_mean_min - 1)
+    num_monthly_std_devs = monthly_std_dev_max - (monthly_std_dev_min - 1)
 
-    placebo_mu    = 0
-    placebo_sigma = 0.05
-    drug_mu       = 0.2
-    drug_sigma    = 0.05
+    theo_placebo_arm_hists = np.zeros((num_monthly_std_devs, num_monthly_means, num_pops))
+    theo_drug_arm_hists    = np.zeros((num_monthly_std_devs, num_monthly_means, num_pops))
+    RR50_emp_stat_powers   = np.zeros(num_pops)
+    
+    for pop_index in range(num_pops):
 
-    num_trials = 10000
+        [theo_placebo_arm_pop_hist, 
+         theo_drug_arm_pop_hist, 
+         RR50_emp_stat_power] = \
+             generate_theo_pop_hists_and_power(monthly_mean_min,
+                                               monthly_mean_max,
+                                               monthly_std_dev_min,
+                                               monthly_std_dev_max,
+                                               num_theo_patients_per_trial_arm,
+                                               num_baseline_months,
+                                               num_testing_months,
+                                               minimum_required_baseline_seizure_count,
+                                               placebo_mu,
+                                               placebo_sigma,
+                                               drug_mu,
+                                               drug_sigma,
+                                               num_trials)
     
-    [theo_placebo_arm_pop_hist, 
-     theo_drug_arm_pop_hist, 
-     RR50_emp_stat_power] = \
-         generate_theo_pop_hists_and_power(monthly_mean_min,
-                                           monthly_mean_max,
-                                           monthly_std_dev_min,
-                                           monthly_std_dev_max,
-                                           num_theo_patients_per_trial_arm,
-                                           num_baseline_months,
-                                           num_testing_months,
-                                           minimum_required_baseline_seizure_count,
-                                           placebo_mu,
-                                           placebo_sigma,
-                                           drug_mu,
-                                           drug_sigma,
-                                           num_trials)
+        theo_placebo_arm_hists[:, :, pop_index] = theo_placebo_arm_pop_hist
+        theo_drug_arm_hists[:, :, pop_index]    = theo_drug_arm_pop_hist
+        RR50_emp_stat_powers[pop_index]         = RR50_emp_stat_power
     
-    
-    
+    store_theo_pop_hists_and_emp_stat_powers(theo_placebo_arm_hists,
+                                             theo_drug_arm_hists,
+                                             RR50_emp_stat_powers,
+                                             data_storage_folder,
+                                             compute_iter)
