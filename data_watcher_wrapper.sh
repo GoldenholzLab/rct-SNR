4ebf50568296e19ab7f9f1fb8cc71421942639eb
Juan <jromero5@bidmc.harvard.edu>
2019-09-30 23:18:33 -0400

    Update data_watcher_wrapper.sh

--- data_watcher_wrapper.sh
+++ data_watcher_wrapper.sh
@@ -10,14 +10,14 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
-inputs[0]=$monthly_mean_min
-inputs[1]=$monthly_mean_max
-inputs[2]=$monthly_std_dev_min
-inputs[3]=$monthly_std_dev_max
+inputs[0]=$1
+inputs[1]=$2
+inputs[2]=$3
+inputs[3]=$4
 
-inputs[4]=$data_storage_folder_name
-inputs[5]=$RR50_stat_power_model_file_name
-inputs[6]=$num_compute_iters
+inputs[4]=$5
+inputs[5]=$6
+inputs[6]=$7
 
 module load gcc/6.2.0
 module load conda2/4.2.13
