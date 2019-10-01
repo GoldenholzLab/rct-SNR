290b1bbdca67a69d7aadf66ee7e3732ac534c0d5
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-01 13:12:35 -0400

    Update data_listener.sh

--- data_listener.sh
+++ data_listener.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/bash
+
+#SBATCH -p short
+#SBATCH -t 0-00:10
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -e jmr95_%j.err
+#SBATCH -o jmr95_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
 inputs[0]=$1
 inputs[1]=$2
@@ -19,4 +29,4 @@ do
 done
 
 echo ${inputs[@]}
-bash local_train_model_wrapper.sh ${inputs[@]}
+sbatch local_train_model_wrapper.sh ${inputs[@]}
