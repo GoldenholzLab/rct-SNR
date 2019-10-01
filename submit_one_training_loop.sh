40c9fc132aa5189232bfb15c1de51b5686d7eecd
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-01 16:38:47 -0400

    Update submit_one_training_loop.sh

--- submit_one_training_loop.sh
+++ submit_one_training_loop.sh
@@ -36,17 +36,14 @@ inputs[0]=$monthly_mean_min
 inputs[1]=$monthly_mean_max
 inputs[2]=$monthly_std_dev_min
 inputs[3]=$monthly_std_dev_max
-
 inputs[4]=$num_theo_patients_per_trial_arm
 inputs[5]=$num_baseline_months
 inputs[6]=$num_testing_months
 inputs[7]=$minimum_required_baseline_seizure_count
-
 inputs[8]=$placebo_mu
 inputs[9]=$placebo_sigma
 inputs[10]=$drug_mu
 inputs[11]=$drug_sigma
-
 inputs[12]=$num_trials
 inputs[13]=$num_pops
 inputs[14]=$data_storage_folder_name
@@ -55,7 +52,6 @@ inputs_two[0]=$monthly_mean_min
 inputs_two[1]=$monthly_mean_max
 inputs_two[2]=$monthly_std_dev_min
 inputs_two[3]=$monthly_std_dev_max
-
 inputs_two[4]=$data_storage_folder_name
 inputs_two[5]=$num_compute_iters
 
@@ -63,7 +59,6 @@ inputs_three[0]=$monthly_mean_min
 inputs_three[1]=$monthly_mean_max
 inputs_three[2]=$monthly_std_dev_min
 inputs_three[3]=$monthly_std_dev_max
-
 inputs_three[4]=$data_storage_folder_name
 inputs_three[5]=$RR50_stat_power_model_file_name
 inputs_three[6]=$num_compute_iters
