e1ec81e7cc20c23cc1165751b35b9d7f49ae7501
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-04 10:25:39 -0400

    Update initialize_model.py

--- main_python_scripts/initialize_model.py
+++ main_python_scripts/initialize_model.py
@@ -3,7 +3,7 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import concatenate
-from keras.layers import Dense
+from keras.layers import Dense, Dropout
 import keras.models as models
 from keras.utils import plot_model
 import sys
@@ -19,7 +19,9 @@ def build_model(num_monthly_means,
     placebo_arm_2D_conv_tensor    = MaxPooling2D((2,2))(placebo_arm_2D_conv_tensor)
     placebo_arm_flattened_tensor  = Flatten()(placebo_arm_2D_conv_tensor)
     placebo_arm_hidden_layer      = Dense(64, activation='relu')(placebo_arm_flattened_tensor)
+    placebo_arm_hidden_layer      = Dropout(0.3)(placebo_arm_hidden_layer)
     placebo_arm_hidden_layer      = Dense(32, activation='relu')(placebo_arm_hidden_layer)
+    placebo_arm_hidden_layer      = Dropout(0.3)(placebo_arm_hidden_layer)
     
     drug_arm_hist_input_tensor = Input(shape=(num_monthly_std_devs, num_monthly_means, 1))
     drug_arm_2D_conv_tensor    = Conv2D(32, (2,2), activation='relu')(drug_arm_hist_input_tensor)
@@ -28,13 +30,19 @@ def build_model(num_monthly_means,
     drug_arm_2D_conv_tensor    = MaxPooling2D((2,2))(drug_arm_2D_conv_tensor)
     drug_arm_flattened_tensor  = Flatten()(drug_arm_2D_conv_tensor)
     drug_arm_hidden_layer      = Dense(64, activation='relu')(drug_arm_flattened_tensor)
+    drug_arm_hidden_layer      = Dropout(0.3)(drug_arm_hidden_layer)
     drug_arm_hidden_layer      = Dense(32, activation='relu')(drug_arm_hidden_layer)
+    drug_arm_hidden_layer      = Dropout(0.3)(drug_arm_hidden_layer)
 
     concatenated_placebo_and_drug_tensor = concatenate([placebo_arm_hidden_layer, drug_arm_hidden_layer])
     pseudo_power_output_tensor = Dense(32, activation='relu')(concatenated_placebo_and_drug_tensor)
+    pseudo_power_output_tensor = Dropout(0.3)(pseudo_power_output_tensor)
     pseudo_power_output_tensor = Dense(16, activation='relu')(pseudo_power_output_tensor)
+    pseudo_power_output_tensor = Dropout(0.3)(pseudo_power_output_tensor)
     pseudo_power_output_tensor = Dense(8, activation='relu')(pseudo_power_output_tensor)
+    pseudo_power_output_tensor = Dropout(0.3)(pseudo_power_output_tensor)
     pseudo_power_output_tensor = Dense(4, activation='relu')(pseudo_power_output_tensor)
+    pseudo_power_output_tensor = Dropout(0.3)(pseudo_power_output_tensor)
     pseudo_power_output_tensor = Dense(1, activation='sigmoid')(pseudo_power_output_tensor)
 
     RR50_stat_power_model = models.Model([placebo_arm_hist_input_tensor, drug_arm_hist_input_tensor], pseudo_power_output_tensor)
