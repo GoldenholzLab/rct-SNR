c72db562493c8696ffcd0310f6ce54e4d184597e
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-04 10:35:40 -0400

    Update submit_one_training_loop.sh

--- submit_one_training_loop.sh
+++ submit_one_training_loop.sh
@@ -72,7 +72,6 @@ do
     sleep 1
     if [ -d ${15} ]
     then
-        echo `pwd`
         x1=`ls -1 ${15}/RR50_emp_stat_powers_*.json | wc -l`
         x2=`ls -1 ${15}/theo_placebo_arm_hists_*.json | wc -l`
         x3=`ls -1 ${15}/theo_drug_arm_hists_*.json | wc -l`
@@ -94,7 +93,6 @@ do
     sleep 1
     if [ -d ${16}]
     then
-        echo `pwd`
         x1=`ls -1 ${16}/RR50_emp_stat_powers_*.json | wc -l`
         x2=`ls -1 ${16}/theo_placebo_arm_hists_*.json | wc -l`
         x3=`ls -1 ${16}/theo_drug_arm_hists_*.json | wc -l`
