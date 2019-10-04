d32b453270b62d534fd41a9a1f220c20183f14a2
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-04 15:31:12 -0400

    Update submit_one_training_loop.sh

--- submit_one_training_loop.sh
+++ submit_one_training_loop.sh
@@ -84,9 +84,9 @@ do
         #echo ' '
         echo ' '
         '
-        x1=`ls -1 "${15}/RR50_emp_stat_powers_"* | wc -l`
-        x2=`ls -1 "${15}/theo_placebo_arm_hists_"* | wc -l`
-        x3=`ls -1 "${15}/theo_drug_arm_hists_"* | wc -l`
+        #x1=`ls -1 "${15}/RR50_emp_stat_powers_"* | wc -l`
+        #x2=`ls -1 "${15}/theo_placebo_arm_hists_"* | wc -l`
+        #x3=`ls -1 "${15}/theo_drug_arm_hists_"* | wc -l`
         echo ' '
         if [ "${x1}" = "$num_compute_training_iters" ]
         then
@@ -119,9 +119,9 @@ do
     sleep 1
     if [ -d ${16} ]
     then
-        x1=`ls -1 "${16}/RR50_emp_stat_powers_"* | wc -l`
-        x2=`ls -1 "${16}/theo_placebo_arm_hists_"* | wc -l`
-        x3=`ls -1 "${16}/theo_drug_arm_hists_"* | wc -l`
+        #x1=`ls -1 "${16}/RR50_emp_stat_powers_"* | wc -l`
+        #x2=`ls -1 "${16}/theo_placebo_arm_hists_"* | wc -l`
+        #x3=`ls -1 "${16}/theo_drug_arm_hists_"* | wc -l`
         if [ "$x1" = "$num_compute_testing_iters" ] && [ "$x2" = "$num_compute_testing_iters" ] && [ "$x3" = "$num_compute_testing_iters" ] && [ -f "RR50_stat_power_model_trained.h5" ]
         then
             all_testing_files_exist='True'
