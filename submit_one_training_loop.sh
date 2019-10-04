4d92eae4f11495a38377b2a62301b48370015f3d
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-04 11:05:12 -0400

    Update submit_one_training_loop.sh

--- submit_one_training_loop.sh
+++ submit_one_training_loop.sh
@@ -70,6 +70,7 @@ do
     sleep 1
     if [ -d ${15} ]
     then
+        echo `pwd`
         x1=`ls -1 "${15}/RR50_emp_stat_powers_*.json" | wc -l`
         x2=`ls -1 "${15}/theo_placebo_arm_hists_*.json" | wc -l`
         x3=`ls -1 "${15}/theo_drug_arm_hists_*.json" | wc -l`
@@ -89,7 +90,7 @@ all_testing_files_exist='False'
 while [ "$all_testing_files_exist" == "False" ]
 do
     sleep 1
-    if [ -d ${16}]
+    if [ -d ${16} ∂]
     then
         x1=`ls -1 "${16}/RR50_emp_stat_powers_*.json" | wc -l`
         x2=`ls -1 "${16}/theo_placebo_arm_hists_*.json" | wc -l`
