2312841f0879556ee0122d11a9f9e6ce5c9843ea
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-07 12:59:17 -0400

    swtiched temporarily back to cpus for now

--- train_model_wrapper.sh
+++ train_model_wrapper.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
-#SBATCH -p gpu
-#SBATCH --gres=gpu:1
+#SBATCH -p short
 #SBATCH --mem=10G
 #SBATCH -t 0-00:05
 #SBATCH -n 1
@@ -25,7 +24,7 @@ inputs[7]=$8
 module load gcc/6.2.0
 module load conda2/4.2.13
 module load python/3.6.0
-module load cuda/9.0
+#module load cuda/9.0
 source activate working_env
 
 srun -c 1 python -u main_python_scripts/train_model.py ${inputs[@]}
