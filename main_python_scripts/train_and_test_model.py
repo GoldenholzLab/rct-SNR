0fcd87525faf2a1115b06ede2d680527b40f4b6f
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-10-10 15:23:27 -0400

    renamed files

--- main_python_scripts/train_and_test_model.py
+++ main_python_scripts/train_and_test_model.py
@@ -1,5 +1,8 @@
+import sys
+import os
 import numpy as np
 import json
+import keras.models as models
 
 
 def load_iter_specific_files(data_storage_folder_name, loop_iter, compute_iter):
@@ -75,4 +78,70 @@ def collect_data_from_folder(num_monthly_means,
     return [theo_placebo_arm_hists, 
             theo_drug_arm_hists, 
             RR50_emp_stat_powers]
+
+
+def take_inputs_from_command_shell():
+
+    monthly_mean_min    = int(sys.argv[1])
+    monthly_mean_max    = int(sys.argv[2])
+    monthly_std_dev_min = int(sys.argv[3])
+    monthly_std_dev_max = int(sys.argv[4])
+
+    training_data_folder_name = sys.argv[5]
+    testing_data_folder_name = sys.argv[5]
+    RR50_stat_power_model_file_name = sys.argv[6]
+    test_RMSEs_file_name = sys.argv[9]
+
+    num_compute_iters = int(sys.argv[7])
+    loop_iter         = int(sys.argv[8])
+
+
+    return [monthly_mean_min, monthly_mean_max, 
+            monthly_std_dev_min, monthly_std_dev_max,
+            training_data_folder_name, testing_data_folder_name, 
+            RR50_stat_power_model_file_name, test_RMSEs_file_name,
+            num_compute_iters, loop_iter]
+
+
+if(__name__=='__main__'):
+
+    [monthly_mean_min, monthly_mean_max, 
+     monthly_std_dev_min, monthly_std_dev_max,
+     training_data_folder_name, testing_data_folder_name, 
+     RR50_stat_power_model_file_name, test_RMSEs_file_name,
+     num_compute_iters, loop_iter] = \
+         take_inputs_from_command_shell()
+
+    num_monthly_means    = monthly_mean_max - (monthly_mean_min - 1)
+    num_monthly_std_devs = monthly_std_dev_max - (monthly_std_dev_min - 1)
+
+    [training_theo_placebo_arm_hists, 
+     training_theo_drug_arm_hists, 
+     training_RR50_emp_stat_powers   ] = \
+         collect_data_from_folder(num_monthly_means,
+                                  num_monthly_std_devs,
+                                  num_compute_iters,
+                                  training_data_folder_name,
+                                  loop_iter)
     
+    RR50_stat_power_model = models.load_model(RR50_stat_power_model_file_name + '_' + str(int(loop_iter)) + '.h5')
+    RR50_stat_power_model.fit([training_theo_placebo_arm_hists, training_theo_drug_arm_hists], training_RR50_emp_stat_powers, epochs=3, batch_size=5)
+
+    [theo_placebo_arm_hists, 
+     theo_drug_arm_hists, 
+     RR50_emp_stat_powers   ] = \
+         collect_data_from_folder(num_monthly_means,
+                                  num_monthly_std_devs,
+                                  num_compute_iters,
+                                  testing_data_folder_name,
+                                  loop_iter)
+    
+    RR50_MSE = RR50_stat_power_model.evaluate([theo_placebo_arm_hists, theo_drug_arm_hists], RR50_emp_stat_powers)
+    RR50_RMSE = np.sqrt(RR50_MSE)
+    print(RR50_RMSE)
+
+    with open(test_RMSEs_file_name + '.txt', 'a') as text_file:
+
+        text_file.write(str(RR50_RMSE) + '\n')
+
+    RR50_stat_power_model.save(RR50_stat_power_model_file_name + '_' + str(int(loop_iter + 1)) + '.h5')
