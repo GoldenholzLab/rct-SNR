f801ebfcc0d9e0f656eafddfd120ee8efbd71833
Juan <jromero5@bidmc.harvard.edu>
2019-10-12 15:17:08 -0400

    Update submit_generate_RR50_AND_MPC_data_wrappers.sh

--- submit_generate_RR50_AND_MPC_data_wrappers.sh
+++ submit_generate_RR50_AND_MPC_data_wrappers.sh
@@ -129,6 +129,7 @@ do
             all_training_block_files_exist="False"
             all_testing_block_files_exist="False"
         fi
+        echo "$block_num"
         echo "$all_training_block_files_exist"
         echo "$all_testing_block_files_exist"
         echo "$all_block_files_exist"
