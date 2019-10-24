0fb8814a6d6ffe0d3d11d6912604763d23306174
Juan <jromero5@bidmc.harvard.edu>
2019-10-24 13:16:33 -0400

    Update generate_TTP_data_wrapper.sh

--- generate_TTP_data_wrapper.sh
+++ generate_TTP_data_wrapper.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p short
 #SBATCH --mem-per-cpu=10G
-#SBATCH -t 0-02:30
+#SBATCH -t 0-02:45
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -e jmr95_%j.err
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
-: '
+
 inputs[0]=$1
 inputs[1]=$2
 inputs[2]=$3
@@ -29,50 +29,6 @@ inputs[14]=${15}
 inputs[15]=${16}
 inputs[16]=${17}
 inputs[17]=${18}
-'
-
-monthly_mean_lower_bound=1
-monthly_mean_upper_bound=16
-monthly_std_dev_lower_bound=1
-monthly_std_dev_upper_bound=16
-
-num_theo_patients_per_trial_arm=153
-num_baseline_months=2
-num_testing_months=3
-minimum_required_baseline_seizure_count=4
-
-placebo_mu=0
-placebo_sigma=0.05
-drug_mu=0.2
-drug_sigma=0.05
-
-num_trials_per_pop=2000
-num_pops=10
-
-data_storage_super_folder_path="/home/jmr95/rct-SNR"
-block_generic_folder_name='generic'
-
-block_num=1
-compute_iter=1
-
-inputs[0]=$monthly_mean_lower_bound
-inputs[1]=$monthly_mean_upper_bound
-inputs[2]=$monthly_std_dev_lower_bound
-inputs[3]=$monthly_std_dev_upper_bound
-inputs[4]=$num_theo_patients_per_trial_arm
-inputs[5]=$num_baseline_months
-inputs[6]=$num_testing_months
-inputs[7]=$minimum_required_baseline_seizure_count
-inputs[8]=$placebo_mu
-inputs[9]=$placebo_sigma
-inputs[10]=$drug_mu
-inputs[11]=$drug_sigma
-inputs[12]=$num_trials_per_pop
-inputs[13]=$num_pops
-inputs[14]=$data_storage_super_folder_path
-inputs[15]=$block_generic_folder_name
-inputs[16]=$block_num
-inputs[17]=$compute_iter
 
 module load gcc/6.2.0
 module load conda2/4.2.13
