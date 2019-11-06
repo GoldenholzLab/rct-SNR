53fbd7c17603aa9bb158e13444f5a097b11b88b0
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-11-06 16:07:47 -0500

    Update cost_submitter.sh
    fixed typo

--- cost_submitter.sh
+++ cost_submitter.sh
@@ -6,8 +6,8 @@ for ((x=1; x<=$num_estims; x=x+1))
 do
     bash RR50_or_MPC_cost_wrapper.sh RR50 smart $x
     bash RR50_or_MPC_cost_wrapper.sh MPC  smart $x
-    bash TTP_cost_wrapper.sh TTP          smart $x
+    bash TTP_cost_wrapper.sh              smart $x
     bash RR50_or_MPC_cost_wrapper.sh RR50 dumb  $x
     bash RR50_or_MPC_cost_wrapper.sh MPC  dumb  $x
-    bash TTP_cost_wrapper.sh TTP          dumb  $x
+    bash TTP_cost_wrapper.sh              dumb  $x
 done
