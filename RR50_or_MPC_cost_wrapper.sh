a8e8e68246d194dcd83acf8e9c380728e137f906
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-11-06 16:22:26 -0500

    created actual O2 scripts

--- RR50_or_MPC_cost_wrapper.sh
+++ RR50_or_MPC_cost_wrapper.sh
@@ -1,6 +1,22 @@
+#!/usr/bin/bash
+
+#SBATCH -p short
+#SBATCH --mem-per-cpu=10G
+#SBATCH -t 0-01:00
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -e jmr95_%j.err
+#SBATCH -o jmr95_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
 inputs[0]=$1
 inputs[1]=$2
 inputs[2]=$3
+
+module load gcc/6.2.0
+module load conda2/4.2.13
+module load python/3.6.0
+source activate working_env
 
 python main_python_scripts/RR50_or_MPC_cost.py ${inputs[@]}
--- cost_submitter.sh
+++ cost_submitter.sh
@@ -1,13 +1,23 @@
 
+#!/usr/bin/bash
+
+#SBATCH -p short
+#SBATCH -t 0-00:05
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -e jmr95_%j.err
+#SBATCH -o jmr95_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
 num_estims=10
 
 for ((x=1; x<=$num_estims; x=x+1))
 do
-    bash RR50_or_MPC_cost_wrapper.sh RR50 smart $x
-    bash RR50_or_MPC_cost_wrapper.sh MPC  smart $x
-    bash TTP_cost_wrapper.sh              smart $x
-    bash RR50_or_MPC_cost_wrapper.sh RR50 dumb  $x
-    bash RR50_or_MPC_cost_wrapper.sh MPC  dumb  $x
-    bash TTP_cost_wrapper.sh              dumb  $x
+    sbatch RR50_or_MPC_cost_wrapper.sh RR50 smart $x
+    sbatch RR50_or_MPC_cost_wrapper.sh MPC  smart $x
+    sbatch TTP_cost_wrapper.sh              smart $x
+    sbatch RR50_or_MPC_cost_wrapper.sh RR50 dumb  $x
+    sbatch RR50_or_MPC_cost_wrapper.sh MPC  dumb  $x
+    sbatch TTP_cost_wrapper.sh              dumb  $x
 done
