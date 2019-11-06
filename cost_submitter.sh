d66f7e9257efd0f5b7625124b0957490bb95a5be
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-11-06 16:55:56 -0500

    Update cost_submitter.sh
    not doing TTP for now

--- cost_submitter.sh
+++ cost_submitter.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/bash
 
 #SBATCH -p short
@@ -10,14 +9,14 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jromero5@bidmc.harvard.edu
 
-num_estims=10
+num_estims=100
 
 for ((x=1; x<=$num_estims; x=x+1))
 do
     sbatch RR50_or_MPC_cost_wrapper.sh RR50 smart $x
     sbatch RR50_or_MPC_cost_wrapper.sh MPC  smart $x
-    sbatch TTP_cost_wrapper.sh              smart $x
+    #sbatch TTP_cost_wrapper.sh              smart $x
     sbatch RR50_or_MPC_cost_wrapper.sh RR50 dumb  $x
     sbatch RR50_or_MPC_cost_wrapper.sh MPC  dumb  $x
-    sbatch TTP_cost_wrapper.sh              dumb  $x
+    #sbatch TTP_cost_wrapper.sh              dumb  $x
 done
