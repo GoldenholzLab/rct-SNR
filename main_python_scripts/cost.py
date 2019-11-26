79aacccf9a5d85d8230e6076fd538b42f65e5688
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-11-26 16:57:12 -0500

    Update cost.py

--- main_python_scripts/cost.py
+++ main_python_scripts/cost.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 import keras.models as models
+import copy
 import sys
 import os
 sys.path.insert(0, os.getcwd())
@@ -85,6 +86,224 @@ def estimate_patient_loc(monthly_mean_min,
     return [monthly_mean_hat, monthly_std_dev_hat]
 
 
+def calculate_SNR(monthly_mean_min,
+                  monthly_mean_max,
+                  monthly_std_dev_min,
+                  monthly_std_dev_max,
+                  current_trial_arm,
+                  num_SNR_iter,
+                  monthly_mean_hat, 
+                  monthly_std_dev_hat,
+                  theo_placebo_arm_patient_pop_params_hat_list,
+                  theo_drug_arm_patient_pop_params_hat_list,
+                  stat_power_model):
+
+    num_monthly_std_devs = monthly_std_dev_max - monthly_std_dev_min + 1
+    num_monthly_means    = monthly_mean_max    - monthly_mean_min    + 1
+
+    keras_formatted_theo_placebo_arm_patient_pop_params_hat_hist     = np.zeros((1, num_monthly_std_devs, num_monthly_means, 1))
+    keras_formatted_theo_drug_arm_patient_pop_params_hat_hist        = np.zeros((1, num_monthly_std_devs, num_monthly_means, 1))
+    keras_formatted_tmp_theo_placebo_arm_patient_pop_params_hat_hist = np.zeros((1, num_monthly_std_devs, num_monthly_means, 1))
+    keras_formatted_tmp_theo_drug_arm_patient_pop_params_hat_hist    = np.zeros((1, num_monthly_std_devs, num_monthly_means, 1))
+
+    tmp_theo_placebo_arm_patient_pop_params_hat_list = \
+        copy.deepcopy(theo_placebo_arm_patient_pop_params_hat_list)
+    tmp_theo_drug_arm_patient_pop_params_hat_list = \
+        copy.deepcopy(theo_placebo_arm_patient_pop_params_hat_list)
+
+    if( current_trial_arm == 'placebo' ):
+        for index in range(num_SNR_iter):
+            tmp_theo_placebo_arm_patient_pop_params_hat_list.append([monthly_mean_hat, monthly_std_dev_hat])
+    elif( current_trial_arm == 'drug' ):
+        for index in range(num_SNR_iter):
+            tmp_theo_drug_arm_patient_pop_params_hat_list.append([monthly_mean_hat, monthly_std_dev_hat])
+
+    keras_formatted_theo_placebo_arm_patient_pop_params_hat_hist[0, :, :, 0] = \
+        convert_theo_pop_hist(monthly_mean_min,
+                              monthly_mean_max,
+                              monthly_std_dev_min,
+                              monthly_std_dev_max,
+                              np.array(theo_placebo_arm_patient_pop_params_hat_list))
+
+    keras_formatted_theo_drug_arm_patient_pop_params_hat_hist[0, :, :, 0] = \
+        convert_theo_pop_hist(monthly_mean_min,
+                              monthly_mean_max,
+                              monthly_std_dev_min,
+                              monthly_std_dev_max,
+                              np.array(theo_drug_arm_patient_pop_params_hat_list))
+
+    keras_formatted_tmp_theo_placebo_arm_patient_pop_params_hat_hist[0, :, :, 0] = \
+        convert_theo_pop_hist(monthly_mean_min,
+                              monthly_mean_max,
+                              monthly_std_dev_min,
+                              monthly_std_dev_max,
+                              np.array(tmp_theo_placebo_arm_patient_pop_params_hat_list))
+
+    keras_formatted_tmp_theo_drug_arm_patient_pop_params_hat_hist[0, :, :, 0] = \
+        convert_theo_pop_hist(monthly_mean_min,
+                              monthly_mean_max,
+                              monthly_std_dev_min,
+                              monthly_std_dev_max,
+                              np.array(tmp_theo_drug_arm_patient_pop_params_hat_list))
+
+    NN_stat_power = \
+        np.squeeze(stat_power_model.predict([keras_formatted_theo_placebo_arm_patient_pop_params_hat_hist, 
+                                             keras_formatted_theo_drug_arm_patient_pop_params_hat_hist]))
+
+    NN_stat_power_w_extra_patient = \
+        np.squeeze(stat_power_model.predict([keras_formatted_tmp_theo_placebo_arm_patient_pop_params_hat_hist, 
+                                             keras_formatted_tmp_theo_drug_arm_patient_pop_params_hat_hist]))
+
+    SNR = NN_stat_power_w_extra_patient - NN_stat_power
+
+    return [SNR, NN_stat_power_w_extra_patient]
+
+
+def smart_algorithm(monthly_mean_min,
+                    monthly_mean_max,
+                    monthly_std_dev_min,
+                    monthly_std_dev_max,
+                    num_baseline_months,
+                    target_stat_power,
+                    num_SNR_iter,
+                    stat_power_model):
+
+    current_trial_arm = 'placebo'
+
+    theo_placebo_arm_patient_pop_params_hat_list = []
+    theo_drug_arm_patient_pop_params_hat_list    = []
+
+    theo_placebo_arm_patient_pop_params_hat_list.append(estimate_patient_loc(monthly_mean_min,
+                                                                             monthly_mean_max - 1,
+                                                                             monthly_std_dev_min,
+                                                                             monthly_std_dev_max - 1,
+                                                                             num_baseline_months))
+
+    theo_drug_arm_patient_pop_params_hat_list.append(estimate_patient_loc(monthly_mean_min,
+                                                                          monthly_mean_max - 1,
+                                                                          monthly_std_dev_min,
+                                                                          monthly_std_dev_max - 1,
+                                                                          num_baseline_months))
+
+    num_patients = 2
+    current_NN_stat_power = 0
+
+    while(current_NN_stat_power < target_stat_power):
+    
+        accepted_patient = False
+
+        while(not accepted_patient):
+
+            [monthly_mean_hat, 
+             monthly_std_dev_hat] = \
+                 estimate_patient_loc(monthly_mean_min,
+                                      monthly_mean_max - 1,
+                                      monthly_std_dev_min,
+                                      monthly_std_dev_max - 1,
+                                      num_baseline_months)
+
+            [SNR, current_NN_stat_power] = \
+                calculate_SNR(monthly_mean_min,
+                              monthly_mean_max,
+                              monthly_std_dev_min,
+                              monthly_std_dev_max,
+                              current_trial_arm,
+                              num_SNR_iter,
+                              monthly_mean_hat, 
+                              monthly_std_dev_hat,
+                              theo_placebo_arm_patient_pop_params_hat_list,
+                              theo_drug_arm_patient_pop_params_hat_list,
+                              stat_power_model)
+
+            if(SNR > 0):
+
+                if( current_trial_arm == 'placebo' ):
+                    theo_placebo_arm_patient_pop_params_hat_list.append([monthly_mean_hat, monthly_std_dev_hat])
+                    current_trial_arm == 'drug' 
+
+                elif( current_trial_arm == 'drug' ):
+                    theo_drug_arm_patient_pop_params_hat_list.append([monthly_mean_hat, monthly_std_dev_hat])
+                    current_trial_arm == 'placebo' 
+
+                num_patients =  num_patients + 1
+            
+                accepted_patient = True
+
+            #print(accepted_patient)
+    
+        #print([np.round(100*current_NN_stat_power, 3), num_patients])
+
+    return num_patients
+
+
+def dumb_algorithm(monthly_mean_min,
+                   monthly_mean_max,
+                   monthly_std_dev_min,
+                   monthly_std_dev_max,
+                   num_baseline_months,
+                   target_stat_power,
+                   num_SNR_iter,
+                   stat_power_model):
+
+    current_trial_arm = 'placebo'
+
+    theo_placebo_arm_patient_pop_params_hat_list = []
+    theo_drug_arm_patient_pop_params_hat_list    = []
+
+    theo_placebo_arm_patient_pop_params_hat_list.append(estimate_patient_loc(monthly_mean_min,
+                                                                             monthly_mean_max - 1,
+                                                                             monthly_std_dev_min,
+                                                                             monthly_std_dev_max - 1,
+                                                                             num_baseline_months))
+
+    theo_drug_arm_patient_pop_params_hat_list.append(estimate_patient_loc(monthly_mean_min,
+                                                                          monthly_mean_max - 1,
+                                                                          monthly_std_dev_min,
+                                                                          monthly_std_dev_max - 1,
+                                                                          num_baseline_months))
+
+    num_patients = 2
+    current_NN_stat_power = 0
+
+    while(current_NN_stat_power < target_stat_power):
+
+        [monthly_mean_hat, 
+         monthly_std_dev_hat] = \
+             estimate_patient_loc(monthly_mean_min,
+                                  monthly_mean_max - 1,
+                                  monthly_std_dev_min,
+                                  monthly_std_dev_max - 1,
+                                  num_baseline_months)
+
+        [_, current_NN_stat_power] = \
+            calculate_SNR(monthly_mean_min,
+                          monthly_mean_max,
+                          monthly_std_dev_min,
+                          monthly_std_dev_max,
+                          current_trial_arm,
+                          num_SNR_iter,
+                          monthly_mean_hat, 
+                          monthly_std_dev_hat,
+                          theo_placebo_arm_patient_pop_params_hat_list,
+                          theo_drug_arm_patient_pop_params_hat_list,
+                          stat_power_model)
+
+        if( current_trial_arm == 'placebo' ):
+            theo_placebo_arm_patient_pop_params_hat_list.append([monthly_mean_hat, monthly_std_dev_hat])
+            current_trial_arm == 'drug' 
+
+        elif( current_trial_arm == 'drug' ):
+            theo_drug_arm_patient_pop_params_hat_list.append([monthly_mean_hat, monthly_std_dev_hat])
+            current_trial_arm == 'placebo' 
+
+        num_patients =  num_patients + 1
+    
+        #print([np.round(100*current_NN_stat_power, 3), num_patients])
+
+    return num_patients
+
+
+'''
 def smart_algorithm(monthly_mean_min,
                     monthly_mean_max,
                     monthly_std_dev_min,
@@ -242,19 +461,64 @@ def dumb_algorithm(monthly_mean_min,
             
 
     return num_theo_patients
+'''
 
 
 if(__name__=='__main__'):
 
     monthly_mean_min = 1
-    monthly_mean_max = 15
+    monthly_mean_max = 16
     monthly_std_dev_min = 1
-    monthly_std_dev_max = 15
+    monthly_std_dev_max = 16
     num_baseline_months = 2
 
-    endpoint_name = 'TTP'
+    endpoint_name = 'MPC'
     generic_stat_power_model_file_name = 'stat_power_model'
     target_stat_power = 0.9
+    num_SNR_iter = 10
+    num_recruitments_processes_for_trials = 100
+
+    stat_power_model_file_path = endpoint_name + '_' + generic_stat_power_model_file_name + '.h5'
+    stat_power_model = models.load_model(stat_power_model_file_path)
+
+    num_smart_algo_patients_array = np.zeros(num_recruitments_processes_for_trials)
+    num_dumb_algo_patients_array  = np.zeros(num_recruitments_processes_for_trials)
+
+    for recruitment_process_index in range(num_recruitments_processes_for_trials):
+
+        num_smart_algo_patients = \
+            smart_algorithm(monthly_mean_min,
+                            monthly_mean_max,
+                            monthly_std_dev_min,
+                            monthly_std_dev_max,
+                            num_baseline_months,
+                            target_stat_power,
+                            num_SNR_iter,
+                            stat_power_model)
+        
+        num_smart_algo_patients_array[recruitment_process_index] = num_smart_algo_patients
+    
+        num_dumb_algo_patients = \
+            dumb_algorithm(monthly_mean_min,
+                           monthly_mean_max,
+                           monthly_std_dev_min,
+                           monthly_std_dev_max,
+                           num_baseline_months,
+                           target_stat_power,
+                           num_SNR_iter,
+                           stat_power_model)
+        
+        num_dumb_algo_patients_array[recruitment_process_index] = num_dumb_algo_patients
+
+        print('\nrecruitment process #' + str(recruitment_process_index + 1) + '\n')
+    
+    expected_num_smart_algo_patients = np.round(np.mean(num_smart_algo_patients_array))
+    expected_num_dumb_algo_patients  = np.round(np.mean(num_dumb_algo_patients_array))
+
+    print([expected_num_smart_algo_patients, expected_num_dumb_algo_patients])
+
+
+'''
     num_recruitments_processes_for_trials = 1000
 
     num_smart_theo_patients_array = np.zeros(num_recruitments_processes_for_trials)
@@ -295,3 +559,4 @@ if(__name__=='__main__'):
     expected_num_dumb_theo_patients  = np.round(np.mean(num_dumb_theo_patients_array))
 
     print([expected_num_smart_theo_patients, expected_num_dumb_theo_patients])
+'''
