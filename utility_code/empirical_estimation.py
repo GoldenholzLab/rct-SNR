1eb3a5d7f76bfbd827ef1dd45319537d357160d5
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-11-27 12:40:51 -0500

    Update empirical_estimation.py
    added new function

--- utility_code/empirical_estimation.py
+++ utility_code/empirical_estimation.py
@@ -3,11 +3,13 @@ from .patient_population_generation import randomly_select_theo_patient_pop
 from .patient_population_generation import generate_theo_patient_pop_params
 from .patient_population_generation import generate_heterogeneous_placebo_arm_patient_pop
 from .patient_population_generation import generate_heterogeneous_drug_arm_patient_pop
+from .patient_population_generation import generate_heterogeneous_patient_pop_per_trial_arm
 from .endpoint_functions import calculate_percent_changes
 from .endpoint_functions import calculate_time_to_prerandomizations
 from .endpoint_functions import calculate_fisher_exact_p_value
 from .endpoint_functions import calculate_Mann_Whitney_U_p_value
 from .endpoint_functions import calculate_logrank_p_value
+from .endpoint_functions import calculate_trial_endpoints
 
 
 def empirically_estimate_RR50_statistical_power(theo_placebo_arm_patient_pop_params,
@@ -210,7 +212,79 @@ def empirically_estimate_TTP_statistical_power(theo_placebo_arm_patient_pop_para
     return TTP_emp_stat_power
 
 
-#def empirically_estimate_all_endpoint_statistical_powers():
+def empirically_estimate_all_endpoint_statistical_powers(num_theo_patients_per_trial_arm,
+                                                         theo_placebo_arm_patient_pop_params,
+                                                         theo_drug_arm_patient_pop_params,
+                                                         num_baseline_months,
+                                                         num_testing_months,
+                                                         minimum_required_baseline_seizure_count,
+                                                         placebo_mu,
+                                                         placebo_sigma,
+                                                         drug_mu,
+                                                         drug_sigma,
+                                                         num_trials):
+
+    RR50_p_value_array = np.zeros(num_trials)
+    MPC_p_value_array  = np.zeros(num_trials)
+    TTP_p_value_array  = np.zeros(num_trials)
+
+    for trial_index in range(num_trials):
+
+        [placebo_arm_baseline_monthly_seizure_diaries,
+         placebo_arm_testing_daily_seizure_diaries,
+         placebo_arm_testing_monthly_seizure_diaries,
+         drug_arm_baseline_monthly_seizure_diaries,
+         drug_arm_testing_daily_seizure_diaries,
+         drug_arm_testing_monthly_seizure_diaries] = \
+             generate_heterogeneous_patient_pop_per_trial_arm(num_theo_patients_per_trial_arm,
+                                                              theo_placebo_arm_patient_pop_params,
+                                                              theo_drug_arm_patient_pop_params,
+                                                              num_baseline_months,
+                                                              num_testing_months,
+                                                              minimum_required_baseline_seizure_count,
+                                                              placebo_mu,
+                                                              placebo_sigma,
+                                                              drug_mu,
+                                                              drug_sigma)
+    
+        [placebo_arm_percent_changes,
+         drug_arm_percent_changes,
+         placebo_arm_TTP_times, 
+         placebo_arm_observed_array,
+         drug_arm_TTP_times, 
+         drug_arm_observed_array] = \
+             calculate_trial_endpoints(num_testing_months,
+                                       num_theo_patients_per_trial_arm,
+                                       placebo_arm_baseline_monthly_seizure_diaries,
+                                       placebo_arm_testing_monthly_seizure_diaries,
+                                       placebo_arm_testing_daily_seizure_diaries,
+                                       drug_arm_baseline_monthly_seizure_diaries,
+                                       drug_arm_testing_monthly_seizure_diaries,
+                                       drug_arm_testing_daily_seizure_diaries)
+
+        RR50_p_value = \
+            calculate_fisher_exact_p_value(placebo_arm_percent_changes,
+                                           drug_arm_percent_changes)
+
+        MPC_p_value = \
+            calculate_Mann_Whitney_U_p_value(placebo_arm_percent_changes,
+                                             drug_arm_percent_changes)
+    
+        TTP_p_value = \
+            calculate_logrank_p_value(placebo_arm_TTP_times, 
+                                      placebo_arm_observed_array, 
+                                      drug_arm_TTP_times, 
+                                      drug_arm_observed_array)
+        
+        RR50_p_value_array[trial_index] = RR50_p_value
+        MPC_p_value_array[trial_index]  = MPC_p_value
+        TTP_p_value_array[trial_index]  = TTP_p_value
+    
+    RR50_stat_power = np.sum(RR50_p_value_array <= 0.05)/num_trials
+    MPC_stat_power  = np.sum(MPC_p_value_array  <= 0.05)/num_trials
+    TTP_stat_power  = np.sum(TTP_p_value_array  <= 0.05)/num_trials
+
+    return [RR50_stat_power, MPC_stat_power, TTP_stat_power]
 
 
 '''
