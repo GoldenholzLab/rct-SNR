645e0ea44ee3e4faed9f33a7038dd2395a9b265d
romero-eng <39012010+romero-eng@users.noreply.github.com>
2019-12-03 15:55:22 -0500

    Update cost_collector.py

--- main_python_scripts/cost_collector.py
+++ main_python_scripts/cost_collector.py
@@ -37,6 +37,10 @@ def calculate_num_patients_and_costs(folder_name,
     average_num_patients_MPC  = np.mean(num_patients_MPC_array)
     average_num_patients_TTP  = np.mean(num_patients_placebo_TTP_array + num_patients_drug_TTP_array)
 
+    std_dev_num_patients_RR50 = np.std(num_patients_RR50_array)
+    std_dev_num_patients_MPC  = np.std(num_patients_MPC_array)
+    std_dev_num_patients_TTP  = np.std(num_patients_placebo_TTP_array + num_patients_drug_TTP_array)
+
     RR50_cost = average_num_patients_RR50*(num_baseline_months + num_testing_months)*1295
     MPC_cost  =  average_num_patients_MPC*(num_baseline_months + num_testing_months)*1295
 
@@ -47,6 +51,9 @@ def calculate_num_patients_and_costs(folder_name,
     return [average_num_patients_RR50, 
             average_num_patients_MPC, 
             average_num_patients_TTP,
+            std_dev_num_patients_RR50,
+            std_dev_num_patients_MPC,
+            std_dev_num_patients_TTP,
             RR50_cost,
             MPC_cost,
             TTP_cost]
@@ -58,6 +65,12 @@ def text_output(dumb_num_patients_RR50,
                 smart_num_patients_RR50,
                 smart_num_patients_MPC,
                 smart_num_patients_TTP,
+                dumb_num_patients_RR50_range,
+                dumb_num_patients_MPC_range,
+                dumb_num_patients_TTP_range,
+                smart_num_patients_RR50_range,
+                smart_num_patients_MPC_range,
+                smart_num_patients_TTP_range,
                 dumb_RR50_cost,
                 dumb_MPC_cost,
                 dumb_TTP_cost,
@@ -72,6 +85,13 @@ def text_output(dumb_num_patients_RR50,
     smart_num_patients_MPC_str  = str(smart_num_patients_MPC)
     smart_num_patients_TTP_str  = str(smart_num_patients_TTP)
 
+    dumb_num_patients_RR50_range_str  = str(dumb_num_patients_RR50_range)
+    dumb_num_patients_MPC_range_str   = str(dumb_num_patients_MPC_range)
+    dumb_num_patients_TTP_range_str   = str(dumb_num_patients_TTP_range)
+    smart_num_patients_RR50_range_str = str(smart_num_patients_RR50_range)
+    smart_num_patients_MPC_range_str  = str(smart_num_patients_MPC_range)
+    smart_num_patients_TTP_range_str  = str(smart_num_patients_TTP_range)
+
     dumb_RR50_cost_str  = str(np.int_(np.round(dumb_RR50_cost)))
     dumb_MPC_cost_str   = str(np.int_(np.round(dumb_MPC_cost)))
     dumb_TTP_cost_str   = str(np.int_(np.round(dumb_TTP_cost)))
@@ -82,14 +102,18 @@ def text_output(dumb_num_patients_RR50,
     MPC_savings_str     = str(np.int_(np.round(dumb_MPC_cost  - smart_MPC_cost)))
     TTP_savings_str     = str(np.int_(np.round(dumb_TTP_cost  - smart_TTP_cost)))
 
-    num_patients_col_str = '        RR50  MPC  TTP'
-    dumb_num_patients_str  = 'dumb:   '  + dumb_num_patients_RR50_str  + ',  ' + dumb_num_patients_MPC_str  + ',  ' + dumb_num_patients_TTP_str
-    smart_num_patients_str = 'smart:  ' + smart_num_patients_RR50_str + ',  ' + smart_num_patients_MPC_str + ',  ' + smart_num_patients_TTP_str
+    num_patients_col_str = '           RR50      MPC      TTP'
+    dumb_num_patients_str  = 'dumb:   ' + dumb_num_patients_RR50_str  + ' ± ' + dumb_num_patients_RR50_range_str + \
+                                  ',  ' + dumb_num_patients_MPC_str   + ' ± ' + dumb_num_patients_MPC_range_str  + \
+                                  ',  ' + dumb_num_patients_TTP_str   + ' ± ' + dumb_num_patients_TTP_range_str
+    smart_num_patients_str = 'smart:  ' + smart_num_patients_RR50_str + ' ± ' + smart_num_patients_RR50_range_str + \
+                                  ',  ' + smart_num_patients_MPC_str  + ' ± ' + smart_num_patients_MPC_range_str  + \
+                                  ',  ' + smart_num_patients_TTP_str  + ' ± ' + smart_num_patients_TTP_range_str
 
-    costs_col_str = '              RR50        MPC         TTP'
-    dumb_costs_str  = 'dumb costs:   $'  + dumb_RR50_cost_str  + ',  $' + dumb_MPC_cost_str  + ',  $' + dumb_TTP_cost_str
-    smart_costs_str = 'smart costs:  $'  + smart_RR50_cost_str + ',  $' + smart_MPC_cost_str + ',  $' + smart_TTP_cost_str
-    savings_str     = 'savings:      $'  + RR50_savings_str    + ',  $' + MPC_savings_str    + ',   $' + TTP_savings_str
+    costs_col_str = '                RR50        MPC       TTP'
+    dumb_costs_str  = 'dumb costs:   $'  + dumb_RR50_cost_str  + ',   $' + dumb_MPC_cost_str  + ',   $' + dumb_TTP_cost_str
+    smart_costs_str = 'smart costs:  $'  + smart_RR50_cost_str + ',   $' + smart_MPC_cost_str + ',   $' + smart_TTP_cost_str
+    savings_str     = 'savings:      $'  + RR50_savings_str    + ',   $' + MPC_savings_str    + ',    $' + TTP_savings_str
 
     output_str = '\n'   + num_patients_col_str  + '\n\n' + dumb_num_patients_str + '\n\n' + smart_num_patients_str + '\n\n' + \
                  '\n\n' + costs_col_str         + '\n\n' + dumb_costs_str        + '\n\n' + smart_costs_str        + '\n\n' + \
@@ -106,6 +130,9 @@ if(__name__=='__main__'):
     [average_dumb_num_patients_RR50, 
      average_dumb_num_patients_MPC, 
      average_dumb_num_patients_TTP,
+     dumb_std_dev_num_patients_RR50,
+     dumb_std_dev_num_patients_MPC,
+     dumb_std_dev_num_patients_TTP,
      dumb_RR50_cost,
      dumb_MPC_cost,
      dumb_TTP_cost] = \
@@ -116,6 +143,9 @@ if(__name__=='__main__'):
     [average_smart_num_patients_RR50, 
      average_smart_num_patients_MPC, 
      average_smart_num_patients_TTP,
+     smart_std_dev_num_patients_RR50,
+     smart_std_dev_num_patients_MPC,
+     smart_std_dev_num_patients_TTP,
      smart_RR50_cost,
      smart_MPC_cost,
      smart_TTP_cost] = \
@@ -130,12 +160,26 @@ if(__name__=='__main__'):
     smart_num_patients_MPC  = np.int_(np.round(average_smart_num_patients_MPC))
     smart_num_patients_TTP  = np.int_(np.round(average_smart_num_patients_TTP))
 
+    dumb_num_patients_RR50_range  = np.int_(np.round(dumb_std_dev_num_patients_RR50))
+    dumb_num_patients_MPC_range   = np.int_(np.round(dumb_std_dev_num_patients_MPC))
+    dumb_num_patients_TTP_range   = np.int_(np.round(dumb_std_dev_num_patients_RR50))
+    smart_num_patients_RR50_range = np.int_(np.round(smart_std_dev_num_patients_RR50))
+    smart_num_patients_MPC_range  = np.int_(np.round(smart_std_dev_num_patients_MPC))
+    smart_num_patients_TTP_range  = np.int_(np.round(smart_std_dev_num_patients_RR50))
+
+
     text_output(dumb_num_patients_RR50,
                 dumb_num_patients_MPC,
                 dumb_num_patients_TTP,
                 smart_num_patients_RR50,
                 smart_num_patients_MPC,
                 smart_num_patients_TTP,
+                dumb_num_patients_RR50_range,
+                dumb_num_patients_MPC_range,
+                dumb_num_patients_TTP_range,
+                smart_num_patients_RR50_range,
+                smart_num_patients_MPC_range,
+                smart_num_patients_TTP_range,
                 dumb_RR50_cost,
                 dumb_MPC_cost,
                 dumb_TTP_cost,
