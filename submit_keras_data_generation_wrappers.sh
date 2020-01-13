6ac8f85b486a22d83e43769b9f223788e0269439
romero-eng <39012010+romero-eng@users.noreply.github.com>
2020-01-13 09:51:42 -0500

    Update submit_keras_data_generation_wrappers.sh

--- submit_keras_data_generation_wrappers.sh
+++ submit_keras_data_generation_wrappers.sh
@@ -35,7 +35,7 @@ num_blocks=46
 num_training_files_per_block=15
 num_testing_files_per_block=5
 
-data_storage_folder_name='/home/jmr95/rct-SNR/keras_data_and_labels_1-4-2020'
+data_storage_folder_name='/home/jmr95/rct-SNR/keras_data_and_labels_1-7-2020'
 
 inputs[0]=$monthly_mean_lower_bound
 inputs[1]=$monthly_mean_upper_bound
